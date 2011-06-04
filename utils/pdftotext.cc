5d9489bf566b700ef38e5e33665a2648b535e1e7
Albert Astals Cid <aacid@kde.org>
2011-06-04 21:46:10 +0100

    forgot to update the C

--- utils/pdftotext.cc
+++ utils/pdftotext.cc
@@ -21,6 +21,7 @@
 // Copyright (C) 2010 Hib Eris <hib@hiberis.nl>
 // Copyright (C) 2010 Kenneth Berland <ken@hero.com>
 // Copyright (C) 2011 Tom Gleason <tom@buildadam.com>
+// Copyright (C) 2011 Steven Murdoch <Steven.Murdoch@cl.cam.ac.uk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
