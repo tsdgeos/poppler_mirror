a128a858c50287cfba74c69996276ea44370dc26
Albert Astals Cid <aacid@kde.org>
2011-08-21 23:52:06 +0200

    Update Adrian's (C)

--- goo/PNGWriter.cc
+++ goo/PNGWriter.cc
@@ -8,7 +8,7 @@
 // Copyright (C) 2009 Shen Liang <shenzhuxi@gmail.com>
 // Copyright (C) 2009 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2009 Stefan Thomas <thomas@eload24.com>
-// Copyright (C) 2010 Adrian Johnson <ajohnson@redneon.com>
+// Copyright (C) 2010, 2011 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2011 Thomas Klausner <wiz@danbala.tuwien.ac.at>
 //
 //========================================================================
