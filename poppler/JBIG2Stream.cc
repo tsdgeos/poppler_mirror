112154567ebcab63959294533b00075d6ca6ea65
Albert Astals Cid <aacid@kde.org>
2011-09-05 19:33:27 +0200

    xpdf303: Initialize to NULL

--- poppler/JBIG2Stream.cc
+++ poppler/JBIG2Stream.cc
@@ -1052,9 +1052,14 @@ private:
 JBIG2SymbolDict::JBIG2SymbolDict(Guint segNumA, Guint sizeA):
   JBIG2Segment(segNumA)
 {
+  Guint i;
+
   size = sizeA;
   bitmaps = (JBIG2Bitmap **)gmallocn_checkoverflow(size, sizeof(JBIG2Bitmap *));
   if (!bitmaps) size = 0;
+  for (i = 0; i < size; ++i) {
+    bitmaps[i] = NULL;
+  }
   genericRegionStats = NULL;
   refinementRegionStats = NULL;
 }
