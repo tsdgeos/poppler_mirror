05ef4227d09381e3a9e8050e447770f44d532386
Albert Astals Cid <aacid@kde.org>
2011-09-05 22:18:33 +0200

    xpdf303: Return false if getLine fails

--- poppler/Stream.cc
+++ poppler/Stream.cc
@@ -444,7 +444,9 @@ GBool ImageStream::getPixel(Guchar *pix) {
   int i;
 
   if (imgIdx >= nVals) {
-    getLine();
+    if (!getLine()) {
+      return gFalse;
+    }
     imgIdx = 0;
   }
   for (i = 0; i < nComps; ++i) {
