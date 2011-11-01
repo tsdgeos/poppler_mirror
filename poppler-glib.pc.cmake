b4896a416b1df14a81b944b3c577fec85c9c7f7d
Rex Dieter <rdieter@math.unl.edu>
2011-11-01 09:40:53 +0100

    Fix pkg-config files
    We were using $FOO_REQUIRED instead of @FOO_REQUIRED@

--- poppler-glib.pc.cmake
+++ poppler-glib.pc.cmake
@@ -6,7 +6,7 @@ includedir=${prefix}/include
 Name: poppler-glib
 Description: GLib wrapper for poppler
 Version: @POPPLER_VERSION@
-Requires: glib-2.0 >= $GLIB_REQUIRED gobject-2.0 >= $GLIB_REQUIRED cairo >= $CAIRO_VERSION
+Requires: glib-2.0 >= @GLIB_REQUIRED@ gobject-2.0 >= @GLIB_REQUIRED@ cairo >= @CAIRO_VERSION@
 @PC_REQUIRES_PRIVATE@
 
 Libs: -L${libdir} -lpoppler-glib
