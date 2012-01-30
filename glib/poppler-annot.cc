be62a4f2e7d5db3b6d85d1b3026979f9561e6e15
Hib Eris <hib@hiberis.nl>
2012-01-30 17:07:28 +0100

    Fix build with latest mingw-w64 headers
    Fixes bug #45407.
    (cherry picked from commit 58c17c3a61f18a6808ec9ba37e82734a655090ac)

--- glib/poppler-annot.cc
+++ glib/poppler-annot.cc
@@ -18,6 +18,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#include "config.h"
 #include "poppler.h"
 #include "poppler-private.h"
 
--- glib/poppler-document.cc
+++ glib/poppler-document.cc
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+#include "config.h"
 #include <string.h>
 
 #ifndef __GI_SCANNER__
