cd5a6a4d02e579e543067d1847164aff90363f16
Thomas Freitag <Thomas.Freitag@kabelmail.de>
2012-02-09 22:01:42 +0100

    [xpdf303] Fix merge bug

--- poppler/GfxState.cc
+++ poppler/GfxState.cc
@@ -5207,7 +5207,7 @@ void GfxImageColorMap::getCMYK(Guchar *x, GfxCMYK *cmyk) {
 
   if (colorSpace2) {
     for (i = 0; i < nComps2; ++i) {
-      color.c[i] = lookup[i][x[0]];
+      color.c[i] = lookup2[i][x[0]];
     }
     colorSpace2->getCMYK(&color, cmyk);
   } else {
