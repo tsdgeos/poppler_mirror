2041d5721871adb2a23999c16ecbdc60abb7b1c4
Albert Astals Cid <aacid@kde.org>
2012-03-15 20:20:31 +0100

    Update C years

--- poppler/GfxFont.h
+++ poppler/GfxFont.h
@@ -20,7 +20,7 @@
 // Copyright (C) 2007 Jeff Muizelaar <jeff@infidigm.net>
 // Copyright (C) 2007 Koji Otani <sho@bbr.jp>
 // Copyright (C) 2011 Axel Strübing <axel.struebing@freenet.de>
-// Copyright (C) 2011 Adrian Johnson <ajohnson@redneon.com>
+// Copyright (C) 2011, 2012 Adrian Johnson <ajohnson@redneon.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
--- poppler/Rendition.cc
+++ poppler/Rendition.cc
@@ -6,6 +6,7 @@
 // Hugo Mercier <hmercier31[at]gmail.com> (c) 2008
 // Pino Toscano <pino@kde.org> (c) 2008
 // Carlos Garcia Campos <carlosgc@gnome.org> (c) 2010
+// Tobias Koenig <tobias.koenig@kdab.com> (c) 2012
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
