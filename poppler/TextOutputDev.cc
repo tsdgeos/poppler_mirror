627e072eb1edbfabf549656e9abf10100ed7340c
Albert Astals Cid <aacid@kde.org>
2012-03-29 21:40:13 +0200

    Update copyrights

--- poppler/TextOutputDev.cc
+++ poppler/TextOutputDev.cc
@@ -28,6 +28,7 @@
 // Copyright (C) 2010 Brian Ewins <brian.ewins@gmail.com>
 // Copyright (C) 2010 Suzuki Toshiya <mpsuzuki@hiroshima-u.ac.jp>
 // Copyright (C) 2011 Sam Liao <phyomh@gmail.com>
+// Copyright (C) 2012 Horst Prote <prote@fmi.uni-stuttgart.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
