b6159fea4a13ecfd1c38b3a666a797c5147dd952
William Bader <williambader@hotmail.com>
2012-05-10 20:02:19 +0200

    splash uses cmykTransferC for M, Y and K in two places

--- splash/Splash.cc
+++ splash/Splash.cc
@@ -15,7 +15,7 @@
 // Copyright (C) 2005 Marco Pesenti Gritti <mpg@redhat.com>
 // Copyright (C) 2010-2012 Thomas Freitag <Thomas.Freitag@alfa.de>
 // Copyright (C) 2010 Christian Feuersänger <cfeuersaenger@googlemail.com>
-// Copyright (C) 2011 William Bader <williambader@hotmail.com>
+// Copyright (C) 2011, 2012 William Bader <williambader@hotmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -400,17 +400,17 @@ void Splash::pipeRun(SplashPipe *pipe) {
       if (state->overprintMask & 2) {
 	pipe->destColorPtr[1] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[1] + state->cmykTransferM[pipe->cSrc[1]], 255) :
-              state->cmykTransferC[pipe->cSrc[1]];
+              state->cmykTransferM[pipe->cSrc[1]];
       }
       if (state->overprintMask & 4) {
 	pipe->destColorPtr[2] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[2] + state->cmykTransferY[pipe->cSrc[2]], 255) :
-              state->cmykTransferC[pipe->cSrc[2]];
+              state->cmykTransferY[pipe->cSrc[2]];
       }
       if (state->overprintMask & 8) {
 	pipe->destColorPtr[3] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[3] + state->cmykTransferK[pipe->cSrc[3]], 255) :
-              state->cmykTransferC[pipe->cSrc[3]];
+              state->cmykTransferK[pipe->cSrc[3]];
       }
       pipe->destColorPtr += 4;
       break;
@@ -848,17 +848,17 @@ void Splash::pipeRunSimpleCMYK8(SplashPipe *pipe) {
   if (state->overprintMask & 2) {
     pipe->destColorPtr[1] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[1] + state->cmykTransferM[pipe->cSrc[1]], 255) :
-              state->cmykTransferC[pipe->cSrc[1]];
+              state->cmykTransferM[pipe->cSrc[1]];
   }
   if (state->overprintMask & 4) {
     pipe->destColorPtr[2] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[2] + state->cmykTransferY[pipe->cSrc[2]], 255) :
-              state->cmykTransferC[pipe->cSrc[2]];
+              state->cmykTransferY[pipe->cSrc[2]];
   }
   if (state->overprintMask & 8) {
     pipe->destColorPtr[3] = (state->overprintAdditive) ? 
               std::min<int>(pipe->destColorPtr[3] + state->cmykTransferK[pipe->cSrc[3]], 255) :
-              state->cmykTransferC[pipe->cSrc[3]];
+              state->cmykTransferK[pipe->cSrc[3]];
   }
   pipe->destColorPtr += 4;
   *pipe->destAlphaPtr++ = 255;
