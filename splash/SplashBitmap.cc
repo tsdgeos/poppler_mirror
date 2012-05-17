87093d1250807f660042295747a012032f84c034
Anthony Wesley <awesley@smartnetworks.com.au>
2012-05-17 19:54:47 +0200

    Fix logic on SplashBitmap::writeImgFile

--- splash/SplashBitmap.cc
+++ splash/SplashBitmap.cc
@@ -20,6 +20,7 @@
 // Copyright (C) 2010 Christian Feuersänger <cfeuersaenger@googlemail.com>
 // Copyright (C) 2010 William Bader <williambader@hotmail.com>
 // Copyright (C) 2011, 2012 Thomas Freitag <Thomas.Freitag@alfa.de>
+// Copyright (C) 2012 Anthony Wesley <awesley@smartnetworks.com.au>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -519,7 +520,7 @@ SplashError SplashBitmap::writeImgFile(ImgWriter *writer, FILE *f, int hDPI, int
     break;
   }
   
-  if (writer->close()) {
+  if (!writer->close()) {
     return splashErrGeneric;
   }
 
