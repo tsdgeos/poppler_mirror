a5adb6738e1e191c64457e0c07ee2e3422a042dd
Albert Astals Cid <aacid@kde.org>
2013-01-19 18:13:33 +0100

    Don't use config.h but poppler-config.h
    That was already included but i forgot to kill the other one

--- poppler/XRef.h
+++ poppler/XRef.h
@@ -14,7 +14,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2005 Brad Hards <bradh@frogmouth.net>
-// Copyright (C) 2006, 2008, 2010-2012 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2006, 2008, 2010-2013 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2007-2008 Julien Rebetez <julienr@svn.gnome.org>
 // Copyright (C) 2007 Carlos Garcia Campos <carlosgc@gnome.org>
 // Copyright (C) 2010 Ilya Gorenbein <igorenbein@finjan.com>
@@ -34,7 +34,6 @@
 #pragma interface
 #endif
 
-#include <config.h>
 #include "poppler-config.h"
 #include "goo/gtypes.h"
 #include "goo/GooMutex.h"
