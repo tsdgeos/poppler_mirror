ebbacd897e2b529d1c3637b6af438b8dcc1d7ce8
Albert Astals Cid <aacid@kde.org>
2013-02-10 11:42:21 +0100

    Update C

--- poppler/Parser.cc
+++ poppler/Parser.cc
@@ -17,6 +17,7 @@
 // Copyright (C) 2006 Krzysztof Kowalczyk <kkowalczyk@gmail.com>
 // Copyright (C) 2009 Ilya Gorenbein <igorenbein@finjan.com>
 // Copyright (C) 2012 Hib Eris <hib@hiberis.nl>
+// Copyright (C) 2013 Adrian Johnson <ajohnson@redneon.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
