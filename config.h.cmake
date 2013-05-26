61f665885daeb0009ecac2cc85188f23d6addb60
Albert Astals Cid <aacid@kde.org>
2013-05-27 00:01:24 +0200

    Fix big file support in cmake
    Tested by Fabio

--- config.h.cmake
+++ config.h.cmake
@@ -222,10 +222,10 @@
 #endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
-/* #undef _FILE_OFFSET_BITS */
+#cmakedefine _FILE_OFFSET_BITS @_FILE_OFFSET_BITS@
 
 /* Define to 1 to make fseeko visible on some hosts (e.g. glibc 2.2). */
-/* #undef _LARGEFILE_SOURCE */
+/* TODO This is wrong, port if needed #undef _LARGEFILE_SOURCE */
 
 /* Define for large files, on AIX-style hosts. */
-/* #undef _LARGE_FILES */
+/* TODO This is wrong, port if needed #undef _LARGE_FILES */
