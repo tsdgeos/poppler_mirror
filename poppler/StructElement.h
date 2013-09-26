8294d18ea96bd18be076bccbdbdaa015fc48aa12
Adrian Perez de Castro <aperez@igalia.com>
2013-09-26 20:46:34 +0300

    Tagged-PDF: Parsing of StructElem standard types and attributes
    Parse attributes and standard types of StructElem nodes of the
    document structure tree. Type name aliases are resolved via the
    RoleMap (and cycles detected). Both standard attributes and user
    properties are mapped to instances of the Attribute class.
    Attributes are parsed both via ClassMap references and directly
    referenced from the StructElem objects.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=64815

--- poppler/StructElement.h
+++ poppler/StructElement.h
@@ -23,7 +23,100 @@
 
 class GooString;
 class Dict;
+class StructElement;
 class StructTreeRoot;
+
+
+class Attribute {
+public:
+  enum Type {
+    Unknown = 0,        // Uninitialized, parsing error, etc.
+    UserProperty,       // User defined attribute (i.e. non-standard)
+
+    // Common standard attributes
+    Placement, WritingMode, BackgroundColor, BorderColor, BorderStyle,
+    BorderThickness, Color, Padding,
+
+    // Block element standard attributes
+    SpaceBefore, SpaceAfter, StartIndent, EndIndent, TextIndent, TextAlign,
+    BBox, Width, Height, BlockAlign, InlineAlign, TBorderStyle, TPadding,
+
+    // Inline element standard attributes
+    BaselineShift, LineHeight, TextDecorationColor, TextDecorationThickness,
+    TextDecorationType, RubyAlign, RubyPosition, GlyphOrientationVertical,
+
+    // Column-only standard attributes
+    ColumnCount, ColumnGap, ColumnWidths,
+
+    // List-only standard attributes
+    ListNumbering,
+
+    // PrintField-only standard attributes
+    Role, checked, Desc,
+
+    // Table-only standard attributes
+    RowSpan, ColSpan, Headers, Scope, Summary,
+  };
+
+  enum Owner {
+    UnknownOwner = 0,
+    // User-defined attributes
+    UserProperties,
+    // Standard attributes
+    Layout, List, PrintField, Table,
+    // Translation to other formats
+    XML_1_00, HTML_3_20, HTML_4_01, OEB_1_00, RTF_1_05, CSS_1_00, CSS_2_00,
+  };
+
+  // Creates a standard attribute. The name is predefined, and the
+  // value is type-checked to conform to the PDF specification.
+  Attribute(Type type, Object *value);
+
+  // Creates an UserProperty attribute, with an arbitrary name and value.
+  Attribute(const char *name, Object *value);
+
+  GBool isOk() const { return type != Unknown; }
+
+  // Name, type and value can be set only on construction.
+  Type getType() const { return type; }
+  Owner getOwner() const { return owner; }
+  const char *getTypeName() const;
+  const char *getOwnerName() const;
+  Object *getValue() const { return &value; }
+  static Object *getDefaultValue(Type type);
+
+  const char *getName() const { return type == UserProperty ? name.getCString() : getTypeName(); }
+
+  // The revision is optional, and defaults to zero.
+  Guint getRevision() const { return revision; }
+  void setRevision(Guint revisionA) { revision = revisionA; }
+
+  // Hidden elements should not be displayed by the user agent
+  GBool isHidden() const { return hidden; }
+  void setHidden(GBool hiddenA) { hidden = hiddenA; }
+
+  // The formatted value may be in the PDF, or be left undefined (NULL).
+  // In the later case the user agent should provide a default representation.
+  const char *getFormattedValue() const { return formatted ? formatted->getCString() : NULL; }
+  void setFormattedValue(const char *formattedA);
+
+  ~Attribute();
+
+private:
+  Type type;
+  Owner owner;
+  Guint revision;
+  mutable GooString name;
+  mutable Object value;
+  GBool hidden;
+  GooString *formatted;
+
+  GBool checkType(StructElement *element = NULL);
+  static Type getTypeForName(const char *name, StructElement *element = NULL);
+  static Attribute *parseUserProperty(Dict *property);
+
+  friend class StructElement;
+};
 
 
 class StructElement {
@@ -42,7 +135,7 @@ public:
 
     P, H, H1, H2, H3, H4, H5, H6,           // Paragraph-like
 
-    L, LI, Lbl,                             // List elements
+    L, LI, Lbl, LBody,                      // List elements
 
     Table, TR, TH, TD, THead, TFoot, TBody, // Table elements
 
@@ -57,6 +150,8 @@ public:
   const char *getTypeName() const;
   Type getType() const { return type; }
   GBool isOk() const { return type != Unknown; }
+  GBool isBlock() const;
+  GBool isInline() const;
 
   inline GBool isContent() const { return (type == MCID) || isObjectRef(); }
   inline GBool isObjectRef() const { return (type == OBJR && c->ref.num != -1 && c->ref.gen != -1); }
@@ -104,6 +199,19 @@ public:
     }
   }
 
+  unsigned getNumAttributes() const { return isContent() ? 0 : s->attributes.size(); }
+  const Attribute *getAttribute(int i) const { return isContent() ? NULL : s->attributes.at(i); }
+  Attribute *getAttribute(int i) { return isContent() ? NULL : s->attributes.at(i); }
+
+  void appendAttribute(Attribute *attribute) {
+    if (!isContent() && attribute) {
+      s->attributes.push_back(attribute);
+    }
+  }
+
+  const Attribute* findAttribute(Attribute::Type attributeType, GBool inherit = gFalse,
+                                 Attribute::Owner owner = Attribute::UnknownOwner) const;
+
   const GooString *getAltText() const { return isContent() ? NULL : s->altText; }
   GooString *getAltText() { return isContent() ? NULL : s->altText; }
 
@@ -113,6 +221,7 @@ public:
   ~StructElement();
 
 private:
+  typedef std::vector<Attribute*>     AttrPtrArray;
   typedef std::vector<StructElement*> ElemPtrArray;
 
   struct StructData {
@@ -125,6 +234,7 @@ private:
     GooString   *language;
     Guint        revision;
     ElemPtrArray elements;
+    AttrPtrArray attributes;
 
     StructData();
     ~StructData();
@@ -159,6 +269,7 @@ private:
   void parse(Dict* elementDict);
   StructElement* parseChild(Object *ref, Object* childObj, std::set<int> &seen);
   void parseChildren(Dict* element, std::set<int> &seen);
+  void parseAttributes(Dict *element, GBool keepExisting = gFalse);
 
   friend class StructTreeRoot;
 };
