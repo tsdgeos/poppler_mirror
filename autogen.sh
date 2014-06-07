bb16c7162c73b871e26f508823c0ee259b7e5420
Hib Eris <hib@hiberis.nl>
2014-06-07 12:00:52 +0200

    Move automake version check from autogen.sh to configure.ac
    https://bugs.freedesktop.org/show_bug.cgi?id=79797

--- autogen.sh
+++ autogen.sh
@@ -6,21 +6,6 @@ srcdir=$(dirname $0)
 
 # Default version requirements
 REQUIRED_GTK_DOC_VERSION=${REQUIRED_GTK_DOC_VERSION:-1.0}
-REQUIRED_AUTOMAKE_VERSION=${REQUIRED_AUTOMAKE_VERSION:-1.7}
-
-case $REQUIRED_AUTOMAKE_VERSION in
-    1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7 automake-1.6 automake-1.5" ;;
-    1.6*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7 automake-1.6" ;;
-    1.7*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8 automake-1.7" ;;
-    1.8*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10 automake-1.9 automake-1.8" ;;
-    1.9*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10 automake-1.9" ;;
-    1.10*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11 automake-1.10" ;;
-    1.11*) automake_progs="automake-1.14 automake-1.13 automake-1.12 automake-1.11" ;;
-    1.12*) automake_progs="automake-1.14 automake-1.13 automake-1.12" ;;
-    1.13*) automake_progs="automake-1.14 automake-1.13" ;;
-    1.14*) automake_progs="automake-1.14" ;;
-esac
 
 # Print types
 boldface="`tput bold 2>/dev/null`"
@@ -114,9 +99,6 @@ if [ ! "`echo $@ |grep -- --enable-gtk-doc`" = "" ]; then
 "http://ftp.gnome.org/pub/GNOME/sources/gtk-doc/" || DIE=1
     want_gtk_doc=true
 fi
-
-version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
-"http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz" || DIE=1
 
 printbold "Running autoreconf -v -i ..."
 (cd $srcdir && autoreconf -v -i )
