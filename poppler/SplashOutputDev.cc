1aa2f6e8a41a6a86dc02bf7c5cbc62355e780961
Albert Astals Cid <aacid@kde.org>
2015-07-17 00:40:56 +0200

    Silly micro optimization: Move vars inside case {}

--- poppler/SplashOutputDev.cc
+++ poppler/SplashOutputDev.cc
@@ -154,40 +154,45 @@ static inline void convertGfxShortColor(SplashColorPtr dest,
                                    SplashColorMode colorMode,
                                    GfxColorSpace *colorSpace,
                                    GfxColor *src) {
-  GfxGray gray;
-  GfxRGB rgb;
-#if SPLASH_CMYK
-  GfxCMYK cmyk;
-  GfxColor deviceN;
-#endif
-
   switch (colorMode) {
     case splashModeMono1:
     case splashModeMono8:
+    {
+      GfxGray gray;
       colorSpace->getGray(src, &gray);
       dest[0] = colToByte(gray);
+    }
     break;
     case splashModeXBGR8:
       dest[3] = 255;
     case splashModeBGR8:
     case splashModeRGB8:
+    {
+      GfxRGB rgb;
       colorSpace->getRGB(src, &rgb);
       dest[0] = colToByte(rgb.r);
       dest[1] = colToByte(rgb.g);
       dest[2] = colToByte(rgb.b);
+    }
     break;
 #if SPLASH_CMYK
     case splashModeCMYK8:
+    {
+      GfxCMYK cmyk;
       colorSpace->getCMYK(src, &cmyk);
       dest[0] = colToByte(cmyk.c);
       dest[1] = colToByte(cmyk.m);
       dest[2] = colToByte(cmyk.y);
       dest[3] = colToByte(cmyk.k);
+    }
     break;
     case splashModeDeviceN8:
+    {
+      GfxColor deviceN;
       colorSpace->getDeviceN(src, &deviceN);
       for (int i = 0; i < SPOT_NCOMPS + 4; i++)
         dest[i] = colToByte(deviceN.c[i]);
+    }
     break;
 #endif
   }
