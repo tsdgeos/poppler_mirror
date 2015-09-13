a4ac855435325225e3d89d83870ff08ec73d86f8
Albert Astals Cid <aacid@kde.org>
2015-09-14 01:52:41 +0200

    PDFDoc::getSignatureWidgets: Do not assume a given page exists

--- poppler/PDFDoc.cc
+++ poppler/PDFDoc.cc
@@ -463,13 +463,16 @@ std::vector<FormWidgetSignature*> PDFDoc::getSignatureWidgets()
   std::vector<FormWidgetSignature*> widget_vector;
 
   for (int i = 1; i <= num_pages; i++) {
-    page_widgets = getCatalog()->getPage(i)->getFormWidgets();
-    for (int j = 0; page_widgets != NULL && j < page_widgets->getNumWidgets(); j++) {
-      if (page_widgets->getWidget(j)->getType() == formSignature) {
-          widget_vector.push_back(static_cast<FormWidgetSignature*>(page_widgets->getWidget(j)));
+    Page *p = getCatalog()->getPage(i);
+    if (p) {
+      page_widgets = p->getFormWidgets();
+      for (int j = 0; page_widgets != NULL && j < page_widgets->getNumWidgets(); j++) {
+	if (page_widgets->getWidget(j)->getType() == formSignature) {
+	    widget_vector.push_back(static_cast<FormWidgetSignature*>(page_widgets->getWidget(j)));
+	}
       }
+      delete page_widgets;
     }
-    delete page_widgets;
   }
   return widget_vector;
 }
