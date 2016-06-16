3829958339b332d7ddf005d5ab98dd0ad3c8b3c6
Albert Astals Cid <aacid@kde.org>
2016-06-16 23:58:07 +0200

    Add (C)

--- poppler/Form.cc
+++ poppler/Form.cc
@@ -7,7 +7,7 @@
 // Copyright 2006-2008 Julien Rebetez <julienr@svn.gnome.org>
 // Copyright 2007-2012, 2015, 2016 Albert Astals Cid <aacid@kde.org>
 // Copyright 2007-2008, 2011 Carlos Garcia Campos <carlosgc@gnome.org>
-// Copyright 2007, 2013 Adrian Johnson <ajohnson@redneon.com>
+// Copyright 2007, 2013, 2016 Adrian Johnson <ajohnson@redneon.com>
 // Copyright 2007 Iñigo Martínez <inigomartinez@gmail.com>
 // Copyright 2008, 2011 Pino Toscano <pino@kde.org>
 // Copyright 2008 Michael Vrable <mvrable@cs.ucsd.edu>
