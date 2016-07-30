7d3aea760a14a73962e429059f73d97391660367
Albert Astals Cid <aacid@kde.org>
2016-07-30 17:32:59 +0200

    Fix abort on documents where the docinfo obj is not a dict
    Bug #97134

--- poppler/PDFDoc.cc
+++ poppler/PDFDoc.cc
@@ -14,7 +14,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2005, 2006, 2008 Brad Hards <bradh@frogmouth.net>
-// Copyright (C) 2005, 2007-2009, 2011-2015 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2005, 2007-2009, 2011-2016 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2008 Julien Rebetez <julienr@svn.gnome.org>
 // Copyright (C) 2008, 2010 Pino Toscano <pino@kde.org>
 // Copyright (C) 2008, 2010, 2011 Carlos Garcia Campos <carlosgc@gnome.org>
@@ -646,7 +646,7 @@ void PDFDoc::setDocInfoStringEntry(const char *key, GooString *value)
 GooString *PDFDoc::getDocInfoStringEntry(const char *key) {
   Object infoObj;
   getDocInfo(&infoObj);
-  if (infoObj.isNull()) {
+  if (!infoObj.isDict()) {
       return NULL;
   }
 
