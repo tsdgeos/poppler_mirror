5d15a52aade68c618c356fe403ca500e74917ef7
Pino Toscano <pino@kde.org>
2016-10-09 13:04:46 +0200

    pdfseparate: remove extra '%' in error message

--- utils/pdfseparate.cc
+++ utils/pdfseparate.cc
@@ -97,7 +97,7 @@ bool extractPages (const char *srcFileName, const char *destFileName) {
     }
   }
   if (!foundmatch && firstPage != lastPage) {
-    error(errSyntaxError, -1, "'{0:s}' must contain '%%d' if more than one page should be extracted", destFileName);
+    error(errSyntaxError, -1, "'{0:s}' must contain '%d' if more than one page should be extracted", destFileName);
     free(auxDestFileName);
     delete doc;
     return false;
