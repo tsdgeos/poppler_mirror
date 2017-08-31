26f64a2a34a9273897c34045ea7af5e8c3c603cb
Albert Astals Cid <aacid@kde.org>
2017-08-31 15:37:07 +0200

    cmake: enable glib for windows too
    There's no need to not having it enabled

--- cmake/modules/FindGLIB.cmake
+++ cmake/modules/FindGLIB.cmake
@@ -13,11 +13,8 @@
 
 include(FindPackageHandleStandardArgs)
 
-if (NOT WIN32)
-  find_package(PkgConfig REQUIRED)
+find_package(PkgConfig REQUIRED)
 
-  pkg_check_modules(GLIB2 "glib-2.0>=${GLIB_REQUIRED}" "gobject-2.0>=${GLIB_REQUIRED}" "gio-2.0>=${GLIB_REQUIRED}")
+pkg_check_modules(GLIB2 "glib-2.0>=${GLIB_REQUIRED}" "gobject-2.0>=${GLIB_REQUIRED}" "gio-2.0>=${GLIB_REQUIRED}")
 
-  find_package_handle_standard_args(GLib DEFAULT_MSG GLIB2_LIBRARIES GLIB2_CFLAGS)
-
-endif(NOT WIN32)
+find_package_handle_standard_args(GLib DEFAULT_MSG GLIB2_LIBRARIES GLIB2_CFLAGS)
