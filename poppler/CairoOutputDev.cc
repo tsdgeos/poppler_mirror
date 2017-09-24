deba5f3c57929a96105d822c8cba46e5334694c7
Carlos Garcia Campos <carlosgc@gnome.org>
2017-09-24 12:31:43 +0200

    cairo: Do not extend the pattern in drawImageMaskRegular
    This is causing some documents with tiling patterns to take ages to render,
    since we switched to use drawImageMaskRegular in 00a536a4. This patch
    applies the same changes made in 7d8dfb09 and db87dc7f for
    drawImageMaskPrescaled to drawImageMaskRegular.

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -2178,9 +2178,6 @@ void CairoOutputDev::drawImageMaskRegular(GfxState *state, Object *ref, Stream *
 
   cairo_pattern_set_filter (pattern, filter);
 
-  if (!printing)
-    cairo_pattern_set_extend (pattern, CAIRO_EXTEND_PAD);
-
   cairo_matrix_init_translate (&matrix, 0, height);
   cairo_matrix_scale (&matrix, width, -height);
   cairo_pattern_set_matrix (pattern, &matrix);
@@ -2196,6 +2193,11 @@ void CairoOutputDev::drawImageMaskRegular(GfxState *state, Object *ref, Stream *
     cairo_save (cairo);
     cairo_rectangle (cairo, 0., 0., 1., 1.);
     cairo_clip (cairo);
+    if (strokePathClip) {
+      cairo_push_group (cairo);
+      fillToStrokePathClip (state);
+      cairo_pop_group_to_source (cairo);
+    }
     cairo_mask (cairo, pattern);
     cairo_restore (cairo);
   } else {
