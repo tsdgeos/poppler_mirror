19909998a2f1f703c9978510ae1fc3baed84de31
Adrian Johnson <ajohnson@redneon.com>
2017-11-11 05:38:25 +1030

    glib demo: fix warning

--- glib/demo/selections.c
+++ glib/demo/selections.c
@@ -337,7 +337,7 @@ pgd_selections_drawing_area_realize (GtkWidget         *area,
 
         gtk_style_context_get_color (style_context, GTK_STATE_FLAG_SELECTED, &rgba);
 	gtk_color_chooser_set_rgba (GTK_COLOR_CHOOSER (demo->fg_color_button), &rgba);
-        gtk_style_context_get_background_color (style_context, GTK_STATE_FLAG_SELECTED, &rgba);
+	gtk_style_context_get (style_context, GTK_STATE_FLAG_SELECTED, "background-color", &rgba, NULL);
 	gtk_color_chooser_set_rgba (GTK_COLOR_CHOOSER (demo->bg_color_button), &rgba);
 }
 
