d63ddd1fb762e536dd78f24bd3e3ddbc3504e1eb
Oliver Sander <oliver.sander@tu-dresden.de>
2017-11-14 20:05:33 +0100

    Use GfxLabColorSpace::transform only when USE_CMS is set
    The recent commit e84338a44f27afb9872cb108fc29683b35ac55f7
    introduced an unqualified use of GfxLabColorSpace::transform,
    even though that member only exists if USE_CMS is set.
    
    Fix this by adding the appropriate preprocessor conditionals.

--- poppler/GfxState.cc
+++ poppler/GfxState.cc
@@ -29,6 +29,7 @@
 // Copyright (C) 2013 Fabio D'Urso <fabiodurso@hotmail.it>
 // Copyright (C) 2015 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2016 Marek Kasik <mkasik@redhat.com>
+// Copyright (C) 2017 Oliver Sander <oliver.sander@tu-dresden.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -1591,7 +1592,9 @@ GfxColorSpace *GfxLabColorSpace::parse(Array *arr, GfxState *state) {
 
   if (!ok) {
       error(errSyntaxWarning, -1, "Bad Lab color space");
+#ifdef USE_CMS
       cs->transform = nullptr;
+#endif
       delete cs;
       return nullptr;
   }
