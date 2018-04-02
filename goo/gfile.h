c19469f2a80ebc09bef3fbd603034e43c4a707cd
Mojca Miklavec <mojca@macports.org>
2018-04-02 12:52:35 +0200

    Fix build on some platforms
    We need to include time.h in gfile.h
    
    Bug #105766

--- goo/gfile.h
+++ goo/gfile.h
@@ -24,6 +24,7 @@
 // Copyright (C) 2014 Peter Breitenlohner <peb@mppmu.mpg.de>
 // Copyright (C) 2017 Christoph Cullmann <cullmann@kde.org>
 // Copyright (C) 2017 Thomas Freitag <Thomas.Freitag@alfa.de>
+// Copyright (C) 2018 Mojca Miklavec <mojca@macports.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -37,6 +38,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <time.h>
 extern "C" {
 #if defined(_WIN32)
 #  include <sys/stat.h>
