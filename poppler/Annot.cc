8fe1a5b96e5ad7dac60dd0f198b3c8b18fb3c5bb
Andre Heinecke <aheinecke@intevation.de>
2018-04-17 10:02:40 +0200

    Fix parsing of focus out actions
    The key is uppercase B with lowercase L. Not I.

--- poppler/Annot.cc
+++ poppler/Annot.cc
@@ -37,6 +37,7 @@
 // Copyright (C) 2015 Tamas Szekeres <szekerest@gmail.com>
 // Copyright (C) 2017 Hans-Ulrich Jüttner <huj@froreich-bioscientia.de>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
+// Copyright 2018 Andre Heinecke <aheinecke@intevation.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
@@ -221,7 +222,7 @@ static LinkAction* getAdditionalAction(Annot::AdditionalActionsType type, Object
                        type == Annot::actionMousePressed ?   "D" :
                        type == Annot::actionMouseReleased ?  "U" :
                        type == Annot::actionFocusIn ?       "Fo" :
-                       type == Annot::actionFocusOut ?      "BI" :
+                       type == Annot::actionFocusOut ?      "Bl" :
                        type == Annot::actionPageOpening ?   "PO" :
                        type == Annot::actionPageClosing ?   "PC" :
                        type == Annot::actionPageVisible ?   "PV" :
