9492eeb6af75d691a2131fa8481556b1ac86795c
Albert Astals Cid <aacid@kde.org>
2018-08-19 23:29:10 +0200

    Update (C)

--- utils/HtmlFonts.cc
+++ utils/HtmlFonts.cc
@@ -28,6 +28,7 @@
 // Copyright (C) 2013 Julien Nabet <serval2412@yahoo.fr>
 // Copyright (C) 2017 Jason Crain <jason@inspiresomeone.us>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
+// Copyright (C) 2018 Steven Boswell <ulatekh@yahoo.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
