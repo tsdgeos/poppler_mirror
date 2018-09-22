tsdgeos/poppler_mirror

commit c792e4cde92e6ece06592955068ffb579e826382

Fix integer overflow by moving check bits-per-compoennt before mask computation. oss-fuzz/9343

poppler/GfxState.cc
@@ -5708,6 +5708,9 @@ GfxImageColorMap::GfxImageColorMap(int bitsA, Object *decode,
   useMatte = gFalse;
 
   // bits per component and color space
+  if (unlikely(bitsA <= 0 || bitsA > 30))
+    goto err1;
+
   bits = bitsA;
   maxPixel = (1 << bits) - 1;
   colorSpace = colorSpaceA;
@@ -5724,9 +5727,6 @@ GfxImageColorMap::GfxImageColorMap(int bitsA, Object *decode,
     lookup2[k] = nullptr;
   }
   byte_lookup = nullptr;
-
-  if (unlikely(bits <= 0))
-    goto err1;
 
   // get decode map
   if (decode->isNull()) {

commit 8a845c65e0dc290af920ce4175689f7321e1d459

Fix crash introduced by c792e4cde92e6ece06592955068ffb579e826382
We need to initialize properly the fields that will be used in the
destructor before bailing out

poppler/GfxState.cc
@@ -5707,19 +5707,7 @@ GfxImageColorMap::GfxImageColorMap(int bitsA, Object *decode,
   ok = gTrue;
   useMatte = gFalse;
 
-  // bits per component and color space
-  if (unlikely(bitsA <= 0 || bitsA > 30))
-    goto err1;
-
-  bits = bitsA;
-  maxPixel = (1 << bits) - 1;
   colorSpace = colorSpaceA;
-
-  // this is a hack to support 16 bits images, everywhere
-  // we assume a component fits in 8 bits, with this hack
-  // we treat 16 bit images as 8 bit ones until it's fixed correctly.
-  // The hack has another part on ImageStream::getLine
-  if (maxPixel > 255) maxPixel = 255;
 
   // initialize
   for (k = 0; k < gfxColorMaxComps; ++k) {
@@ -5727,6 +5715,19 @@ GfxImageColorMap::GfxImageColorMap(int bitsA, Object *decode,
     lookup2[k] = nullptr;
   }
   byte_lookup = nullptr;
+
+  // bits per component and color space
+  if (unlikely(bitsA <= 0 || bitsA > 30))
+    goto err1;
+
+  bits = bitsA;
+  maxPixel = (1 << bits) - 1;
+
+  // this is a hack to support 16 bits images, everywhere
+  // we assume a component fits in 8 bits, with this hack
+  // we treat 16 bit images as 8 bit ones until it's fixed correctly.
+  // The hack has another part on ImageStream::getLine
+  if (maxPixel > 255) maxPixel = 255;
 
   // get decode map
   if (decode->isNull()) {