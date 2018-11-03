2cd0ded91ad25a655ddd0c3ff14bc8c25c4eb410
Albert Astals Cid <aacid@kde.org>
2018-11-03 01:43:30 +0100

    Update (C)

--- poppler/XRef.h
+++ poppler/XRef.h
@@ -24,6 +24,7 @@
 // Copyright (C) 2013, 2017 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2016 Jakub Alba <jakubalba@gmail.com>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2018 Marek Kasik <mkasik@redhat.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
