6912e06d9ab19ba28991b5cab3319d61d856bd6d
Adam Reichold <adam.reichold@t-online.de>
2018-11-06 09:00:02 +0100

    Check for stream before calling stream methods when saving an embedded file.
    Closes #659

--- poppler/FileSpec.cc
+++ poppler/FileSpec.cc
@@ -93,6 +93,9 @@ bool EmbFile::save(const char *path) {
 bool EmbFile::save2(FILE *f) {
   int c;
 
+  if (unlikely(!m_objStr.isStream()))
+    return false;
+
   m_objStr.streamReset();
   while ((c = m_objStr.streamGetChar()) != EOF) {
     fputc(c, f);
