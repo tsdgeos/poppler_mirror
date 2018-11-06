744a3491675f22e36a5fba38f58bb90ec7291884
Albert Astals Cid <aacid@kde.org>
2018-11-06 19:28:22 +0100

    Update (C)

--- poppler/FileSpec.cc
+++ poppler/FileSpec.cc
@@ -10,6 +10,7 @@
 // Copyright (C) 2012, 2017, 2018 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2012 Hib Eris <hib@hiberis.nl>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
+// Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
