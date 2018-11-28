dc00f29828feb4192df59b1c1be783d631c2ef5a
Albert Astals Cid <aacid@kde.org>
2018-11-28 20:06:20 +0100

    Update (C)

--- goo/GooString.cc
+++ goo/GooString.cc
@@ -28,6 +28,7 @@
 // Copyright (C) 2016 Jakub Alba <jakubalba@gmail.com>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2018 Greg Knight <lyngvi@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
