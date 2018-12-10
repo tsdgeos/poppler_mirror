859b4e54c8a6af13eb4715076dc90a17bb2b71df
Alexey Pavlov <alexpux@gmail.com>
2018-12-10 01:06:18 +0100

    msys2: support enabling NSS on mingw-w64

--- cmake/modules/FindNSS3.cmake
+++ cmake/modules/FindNSS3.cmake
@@ -12,11 +12,11 @@
 
 include(FindPackageHandleStandardArgs)
 
-if (NOT WIN32)
+if (NOT MSVC)
   find_package(PkgConfig REQUIRED)
 
   pkg_check_modules(NSS3 "nss>=3.19")
 
   find_package_handle_standard_args(NSS3 DEFAULT_MSG NSS3_LIBRARIES NSS3_CFLAGS)
 
-endif(NOT WIN32)
+endif(NOT MSVC)
