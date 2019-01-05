2cebd37e2910a7edacb19c32b82d8b8f7cfe41d6
Albert Astals Cid <aacid@kde.org>
2019-01-05 17:31:00 +0100

    Update (C) of previous commits

--- poppler/CairoFontEngine.cc
+++ poppler/CairoFontEngine.cc
@@ -29,6 +29,7 @@
 // Copyright (C) 2013 Thomas Freitag <Thomas.Freitag@alfa.de>
 // Copyright (C) 2015, 2016 Jason Crain <jason@aquaticape.us>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2019 Christian Persch <chpe@src.gnome.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
--- splash/SplashBitmap.cc
+++ splash/SplashBitmap.cc
@@ -24,6 +24,7 @@
 // Copyright (C) 2015, 2018 Adam Reichold <adamreichold@myopera.com>
 // Copyright (C) 2016 Kenji Uno <ku@digitaldolphins.jp>
 // Copyright (C) 2018 Martin Packman <gzlist@googlemail.com>
+// Copyright (C) 2019 Christian Persch <chpe@src.gnome.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
