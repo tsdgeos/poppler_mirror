e65dfd791447225c9477f973cbc172b269164af9
Albert Astals Cid <aacid@kde.org>
2019-02-10 18:01:25 +0100

    NameTree: Remove unused member

--- poppler/Catalog.h
+++ poppler/Catalog.h
@@ -14,7 +14,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2005 Kristian Høgsberg <krh@redhat.com>
-// Copyright (C) 2005, 2007, 2009-2011, 2013, 2017, 2018 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2005, 2007, 2009-2011, 2013, 2017-2019 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2005 Jonathan Blandford <jrb@redhat.com>
 // Copyright (C) 2005, 2006, 2008 Brad Hards <bradh@frogmouth.net>
 // Copyright (C) 2007 Julien Rebetez <julienr@svn.gnome.org>
@@ -92,7 +92,6 @@ private:
   void addEntry(Entry *entry);
 
   XRef *xref;
-  Object *root;
   Entry **entries;
   int size, length; // size is the number of entries in
                     // the array of Entry*
