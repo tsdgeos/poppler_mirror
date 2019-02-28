c33a0e82f8df11f7a5a0f7d0e0020c485b9142f0
Albert Astals Cid <aacid@kde.org>
2019-02-28 17:33:40 +0100

    Update (C) of previous commit

--- poppler/FileSpec.h
+++ poppler/FileSpec.h
@@ -6,7 +6,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2008 Carlos Garcia Campos <carlosgc@gnome.org>
-// Copyright (C) 2017, 2018 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2017-2019 Albert Astals Cid <aacid@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
