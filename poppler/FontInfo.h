4ae6786469ec0268307fa05cb6d9ce58b4541941
Albert Astals Cid <aacid@kde.org>
2019-04-08 00:56:12 +0200

    Update (C) of previous commit

--- poppler/FontInfo.h
+++ poppler/FontInfo.h
@@ -9,6 +9,7 @@
 // Copyright (C) 2012 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2013 Thomas Freitag <Thomas.Freitag@alfa.de>
 // Copyright (C) 2019 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2019 Adam Reichold <adam.reichold@t-online.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
