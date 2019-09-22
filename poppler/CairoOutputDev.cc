1ef3b513164640390ffeb765ec0dcfa8d36f7262
Albert Astals Cid <aacid@kde.org>
2019-09-22 11:33:31 +0200

    Update (C) of last commit

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -31,6 +31,7 @@
 // Copyright (C) 2015 Suzuki Toshiya <mpsuzuki@hiroshima-u.ac.jp>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2019 Marek Kasik <mkasik@redhat.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
