c017531fdebdd8a9d56c86ba89e861cc067000a2
Albert Astals Cid <aacid@kde.org>
2019-11-24 09:52:54 +0100

    Update (C) of few past commits

--- qt5/demos/navigationtoolbar.cpp
+++ qt5/demos/navigationtoolbar.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2008-2009, Pino Toscano <pino@kde.org>
  * Copyright (C) 2013, Fabio D'Urso <fabiodurso@hotmail.it>
+ * Copyright (C) 2019, Albert Astals Cid <aacid@kde.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
