2ad55c01e03a3fdb287dae73dbd37849498fa329
Albert Astals Cid <aacid@kde.org>
2019-12-01 22:42:24 +0100

    Update (C) of last commit

--- poppler/JBIG2Stream.h
+++ poppler/JBIG2Stream.h
@@ -19,6 +19,7 @@
 // Copyright (C) 2019 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
 // Copyright (C) 2019 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2019 Even Rouault <even.rouault@spatialys.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
