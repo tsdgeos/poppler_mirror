0d31745f9f2ce6fe0ff01da507c13c393c4456a3
Albert Astals Cid <aacid@kde.org>
2020-01-12 22:30:24 +0100

    Update (C) of last commit

--- poppler/GlobalParamsWin.cc
+++ poppler/GlobalParamsWin.cc
@@ -9,7 +9,7 @@
    // Copyright (C) 2013, 2018, 2019 Adam Reichold <adamreichold@myopera.com>
    // Copyright (C) 2013 Dmytro Morgun <lztoad@gmail.com>
    // Copyright (C) 2017 Christoph Cullmann <cullmann@kde.org>
-   // Copyright (C) 2017, 2018 Albert Astals Cid <aacid@kde.org>
+   // Copyright (C) 2017, 2018, 2020 Albert Astals Cid <aacid@kde.org>
    // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
    // Copyright (C) 2019 Christian Persch <chpe@src.gnome.org>
    // Copyright (C) 2019 Oliver Sander <oliver.sander@tu-dresden.de>
