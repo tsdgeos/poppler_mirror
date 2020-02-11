5539aeba90d8e5b2ea78444495deaefc900d299c
Albert Astals Cid <aacid@kde.org>
2020-02-11 23:05:30 +0100

    Update (C)

--- utils/pdfdetach.cc
+++ utils/pdfdetach.cc
@@ -19,6 +19,7 @@
 // Copyright (C) 2018, 2020 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2019 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2020 <r.coeffier@bee-buzziness.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
