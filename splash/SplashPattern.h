bad6503c276f306170799cab2e98f53894c6e147
Albert Astals Cid <aacid@kde.org>
2020-02-28 00:35:25 +0100

    Update (C) of last commit

--- splash/SplashPattern.h
+++ splash/SplashPattern.h
@@ -12,7 +12,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2010, 2011, 2014 Thomas Freitag <Thomas.Freitag@alfa.de>
-// Copyright (C) 2018 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2018, 2020 Albert Astals Cid <aacid@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
