0f01638f044c247c2591f873f9f7558ed3c3b4ce
Albert Astals Cid <aacid@kde.org>
2020-05-13 23:12:43 +0200

    Update (C)

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -31,7 +31,7 @@
 // Copyright (C) 2015 Suzuki Toshiya <mpsuzuki@hiroshima-u.ac.jp>
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
 // Copyright (C) 2018, 2020 Adam Reichold <adam.reichold@t-online.de>
-// Copyright (C) 2019 Marek Kasik <mkasik@redhat.com>
+// Copyright (C) 2019, 2020 Marek Kasik <mkasik@redhat.com>
 // Copyright (C) 2020 Michal <sudolskym@gmail.com>
 // Copyright (C) 2020 Oliver Sander <oliver.sander@tu-dresden.de>
 //
