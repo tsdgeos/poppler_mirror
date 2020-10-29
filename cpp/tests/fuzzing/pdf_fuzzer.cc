919a71e52110774f461cbe079a5ed65cf8f48c91
Ceyhun Alp <ceyhunalp@google.com>
2020-10-29 23:32:15 +0000

    Updating pdf_fuzzer with the version from OSS-Fuzz repository

--- cpp/tests/fuzzing/pdf_fuzzer.cc
+++ cpp/tests/fuzzing/pdf_fuzzer.cc
@@ -18,22 +18,27 @@
 
 #include <cstdint>
 
+#include <poppler-destination.h>
 #include <poppler-global.h>
 #include <poppler-document.h>
 #include <poppler-page.h>
 #include <poppler-page-renderer.h>
 
-static void dummy_error_function(const std::string &, void *) { }
+static void nop_func(const std::string &msg, void *) {};
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    poppler::set_debug_error_function(dummy_error_function, nullptr);
+    poppler::set_debug_error_function(nop_func, nullptr);
 
     poppler::document *doc = poppler::document::load_from_raw_data((const char *)data, size);
     if (!doc || doc->is_locked()) {
         delete doc;
         return 0;
     }
+    doc->metadata();
+    doc->create_destination_map();
+    doc->embedded_files();
+    doc->fonts();
 
     poppler::page_renderer r;
     for (int i = 0; i < doc->pages(); i++) {
@@ -42,6 +47,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
             continue;
         }
         r.render_page(p);
+        p->text_list(poppler::page::text_list_include_font);
         delete p;
     }
 
