f4d640d7ad00b30797acce47299cf7a7a59ad505
Kyle Auble <kyle.auble@outlook.com>
2020-11-12 19:46:15 -0500

    cmake: Remove redundant unit-test macro
    poppler_add_unittest was only used 3 times & effectively differed from
    poppler_add_test by just a CMake add_test call.

--- cmake/modules/PopplerMacros.cmake
+++ cmake/modules/PopplerMacros.cmake
@@ -5,6 +5,8 @@
 
 macro(POPPLER_ADD_TEST exe build_flag)
   set(build_test ${${build_flag}})
+
+  # Omit the disabled test binaries from the "all" target
   if(NOT build_test)
     set(_add_executable_param ${_add_executable_param} EXCLUDE_FROM_ALL)
   endif(NOT build_test)
@@ -12,6 +14,7 @@ macro(POPPLER_ADD_TEST exe build_flag)
   add_executable(${exe} ${_add_executable_param} ${ARGN})
 
   # if the tests are EXCLUDE_FROM_ALL, add a target "buildtests" to build all tests
+  # Don't try to use custom targets if building with Visual Studio
   if(NOT build_test AND NOT MSVC_IDE)
     get_property(_buildtestsAdded GLOBAL PROPERTY BUILDTESTS_ADDED)
     if(NOT _buildtestsAdded)
@@ -21,26 +24,6 @@ macro(POPPLER_ADD_TEST exe build_flag)
     add_dependencies(buildtests ${exe})
   endif(NOT build_test AND NOT MSVC_IDE)
 endmacro(POPPLER_ADD_TEST)
-
-macro(POPPLER_ADD_UNITTEST exe build_flag)
-  set(build_test ${${build_flag}})
-  if(NOT build_test)
-    set(_add_executable_param ${_add_executable_param} EXCLUDE_FROM_ALL)
-  endif(NOT build_test)
-
-  add_executable(${exe} ${_add_executable_param} ${ARGN})
-  add_test(${exe} ${EXECUTABLE_OUTPUT_PATH}/${exe})
-
-  # if the tests are EXCLUDE_FROM_ALL, add a target "buildtests" to build all tests
-  if(NOT build_test)
-    get_property(_buildtestsAdded GLOBAL PROPERTY BUILDTESTS_ADDED)
-    if(NOT _buildtestsAdded)
-      add_custom_target(buildtests)
-      set_property(GLOBAL PROPERTY BUILDTESTS_ADDED TRUE)
-    endif(NOT _buildtestsAdded)
-    add_dependencies(buildtests ${exe})
-  endif(NOT build_test)
-endmacro(POPPLER_ADD_UNITTEST)
 
 macro(POPPLER_CREATE_INSTALL_PKGCONFIG generated_file install_location)
   configure_file(${generated_file}.cmake ${CMAKE_CURRENT_BINARY_DIR}/${generated_file} @ONLY)
