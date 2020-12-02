fbdf0cfc26cb686677e865b109a283b20231a2d3
Albert Astals Cid <aacid@kde.org>
2020-12-02 01:05:16 +0100

    typo

--- splash/Splash.cc
+++ splash/Splash.cc
@@ -2829,7 +2829,7 @@ void Splash::arbitraryTransformMask(SplashImageMaskSource src, void *srcData, in
     vx[3] = mat[0] + mat[4];
     vy[3] = mat[1] + mat[5];
 
-    // make sure cx/vy fit in integers since we're transforming them to in the next lines
+    // make sure vx/vy fit in integers since we're transforming them to in the next lines
     for (i = 0; i < 4; ++i) {
         if (unlikely(vx[i] < INT_MIN || vx[i] > INT_MAX || vy[i] < INT_MIN || vy[i] > INT_MAX)) {
             error(errInternal, -1, "arbitraryTransformMask vertices values don't fit in an integer");
