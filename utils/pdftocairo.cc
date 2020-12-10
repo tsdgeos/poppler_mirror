009ea8cbd43736594a1e598e68ffd0e0a0983d81
Albert Astals Cid <aacid@kde.org>
2020-12-11 00:29:40 +0100

    Update (C)

--- utils/pdftocairo.cc
+++ utils/pdftocairo.cc
@@ -36,6 +36,7 @@
 // Copyright (C) 2019 Kris Jurka <jurka@ejurka.com>
 // Copyright (C) 2020 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2020 Philipp Knechtges <philipp-dev@knechtges.com>
+// Copyright (C) 2020 Salvo Miosi <salvo.ilmiosi@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
