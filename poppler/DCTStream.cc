ae614bf8ab42c9d0c7ac57ecdfdcbcfc4ff6c639
Albert Astals Cid <aacid@kde.org>
2020-12-20 23:15:12 +0100

    Fix DCTStream::getChars we're reading past the buffer check
    I wonder how this had never crashed before :S
    
    Fixes #1011

--- poppler/DCTStream.cc
+++ poppler/DCTStream.cc
@@ -218,8 +218,8 @@ int DCTStream::getChars(int nChars, unsigned char *buffer)
                 return i;
         }
         int left = limit - current;
-        if (nChars < left)
-            left = nChars;
+        if (left + i > nChars)
+            left = nChars - i;
         memcpy(buffer + i, current, left);
         current += left;
         i += left;
