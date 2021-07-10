e2f7f5e8eae0cb13d88af4400d68697c6e6bf5ed
Nelson Benítez León <nbenitezl@gmail.com>
2021-07-10 00:13:46 -0400

    Add glib test for issue #1100

--- glib/tests/check_text.c
+++ glib/tests/check_text.c
@@ -16,6 +16,8 @@ int main(int argc, char *argv[])
     GFile *infile;
     PopplerDocument *doc;
     PopplerPage *page;
+    PopplerRectangle *areas = NULL;
+    guint n_glyph_areas, n_utf8_chars;
     int npages, n;
     char *text;
     GError *err = NULL;
@@ -48,7 +50,46 @@ int main(int argc, char *argv[])
     text = poppler_page_get_text(page);
     g_print("%s\n", text);
     g_assert_cmpstr(text, ==, "The slow brown fox jumps over the black dog.");
-    g_object_unref(page);
+
+    /* Cleanup vars for next test */
+    g_clear_object(&page);
+    g_clear_object(&doc);
+    g_clear_object(&infile);
+    g_clear_pointer(&text, g_free);
+
+    /* Test for consistency between utf8 characters returned by poppler_page_get_text()
+     * and glyph layout areas returned by poppler_page_get_text_layout(). Issue #1100 */
+    g_print("Consistency test between poppler_page_get_text() and poppler_page_get_text_layout()\n");
+    g_print("Issue #1100 \n");
+    infile = g_file_new_for_path(TESTDATADIR "/unittestcases/searchAcrossLines.pdf");
+    if (!infile)
+        exit(EXIT_FAILURE);
+
+    doc = poppler_document_new_from_gfile(infile, NULL, NULL, &err);
+    if (doc == NULL) {
+        g_printerr("error opening pdf file: %s\n", err->message);
+        g_error_free(err);
+        exit(EXIT_FAILURE);
+    }
+
+    page = poppler_document_get_page(doc, 0);
+    if (page == NULL || !POPPLER_IS_PAGE(page)) {
+        g_print("error opening pdf page\n");
+        exit(EXIT_FAILURE);
+    }
+
+    text = poppler_page_get_text(page);
+    n_utf8_chars = (guint)g_utf8_strlen(text, -1);
+    poppler_page_get_text_layout(page, &areas, &n_glyph_areas);
+    g_assert_cmpuint(n_glyph_areas, ==, n_utf8_chars);
+    g_print("Test: OK ('layout glyph areas' match amount of 'utf8 characters')\n");
+
+    /* Cleanup vars for next test */
+    g_clear_object(&page);
+    g_clear_object(&doc);
+    g_clear_object(&infile);
+    g_clear_pointer(&areas, g_free);
+    g_clear_pointer(&text, g_free);
 
     return EXIT_SUCCESS;
 }
