e4bf68a684590ab17a7342030fd863dc15d3f507
Albert Astals Cid <aacid@kde.org>
2022-01-11 00:31:05 +0100

    Add (C)

--- poppler/Annot.h
+++ poppler/Annot.h
@@ -41,6 +41,7 @@
 // Copyright (C) 2021 Zachary Travis <ztravis@everlaw.com>
 // Copyright (C) 2021 Mahmoud Ahmed Khalil <mahmoudkhalil11@gmail.com>
 // Copyright (C) 2021 Georgiy Sgibnev <georgiy@sgibnev.com>. Work sponsored by lab50.net.
+// Copyright (C) 2022 Martin <martinbts@gmx.net>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
