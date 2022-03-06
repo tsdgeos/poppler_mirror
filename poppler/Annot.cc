f5ff8e1a96a40688221cc2800afb17b3ee5c7063
Albert Astals Cid <aacid@kde.org>
2022-03-06 17:08:13 +0100

    Update (C)

--- poppler/Annot.cc
+++ poppler/Annot.cc
@@ -53,6 +53,7 @@
 // Copyright (C) 2021 Mahmoud Ahmed Khalil <mahmoudkhalil11@gmail.com>
 // Copyright (C) 2021 Georgiy Sgibnev <georgiy@sgibnev.com>. Work sponsored by lab50.net.
 // Copyright (C) 2022 Martin <martinbts@gmx.net>
+// Copyright (C) 2022 Andreas Naumann <42870-ANaumann85@users.noreply.gitlab.freedesktop.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
