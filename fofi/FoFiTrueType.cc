faa45adef0c4745ca4e8ba784e1559fda5e1fa8f
Albert Astals Cid <aacid@kde.org>
2022-03-15 00:03:41 +0100

    Update (C) from last commit

--- fofi/FoFiTrueType.cc
+++ fofi/FoFiTrueType.cc
@@ -24,6 +24,7 @@
 // Copyright (C) 2015 Aleksei Volkov <Aleksei Volkov>
 // Copyright (C) 2015, 2016 William Bader <williambader@hotmail.com>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2022 Zachary Travis <ztravis@everlaw.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
