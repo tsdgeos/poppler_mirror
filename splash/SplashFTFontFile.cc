4f1f202f9e4db08f6dd2ba9c6a96decbefb10899
Albert Astals Cid <aacid@kde.org>
2022-03-15 15:56:42 +0100

    Update (C)

--- splash/SplashFTFontFile.cc
+++ splash/SplashFTFontFile.cc
@@ -13,7 +13,7 @@
 //
 // Copyright (C) 2006 Takashi Iwai <tiwai@suse.de>
 // Copyright (C) 2014, 2017 Adrian Johnson <ajohnson@redneon.com>
-// Copyright (C) 2017, 2018 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2017, 2018, 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2018 Albert Astals Cid <aacid@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
