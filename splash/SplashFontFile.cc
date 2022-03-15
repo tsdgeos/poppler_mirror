tsdgeos/poppler_mirror

commit efe7d77838896ddcefb838f382628ae31be54d99

Use std::string for filename in SplashFontSrc

splash/SplashFontFile.cc
@@ -80,10 +80,6 @@ SplashFontSrc::~SplashFontSrc()
             gfree(buf);
         }
     }
-
-    if (isFile && fileName) {
-        delete fileName;
-    }
 }
 
 void SplashFontSrc::ref()
@@ -98,10 +94,10 @@ void SplashFontSrc::unref()
     }
 }
 
-void SplashFontSrc::setFile(const GooString *file)
+void SplashFontSrc::setFile(const std::string &file)
 {
     isFile = true;
-    fileName = file->copy();
+    fileName = file;
 }
 
 void SplashFontSrc::setBuf(char *bufA, int bufLenA)

poppler/SplashOutputDev.cc
@@ -1894,7 +1894,7 @@ reload:
         }
 
         // embedded font
-        const GooString *fileName = nullptr;
+        std::string fileName;
 
         if (fontLoc->locType == gfxFontLocEmbedded) {
             // if there is an embedded font, read it to memory
@@ -1905,13 +1905,13 @@ reload:
 
             // external font
         } else { // gfxFontLocExternal
-            fileName = fontLoc->pathAsGooString();
+            fileName = fontLoc->path;
             fontType = fontLoc->fontType;
             doAdjustFontMatrix = true;
         }
 
         fontsrc = new SplashFontSrc;
-        if (fileName) {
+        if (!fileName.empty()) {
             fontsrc->setFile(fileName);
         } else {
             fontsrc->setBuf(tmpBuf, tmpBufLen);
@@ -1949,8 +1949,8 @@ reload:
         case fontTrueType:
         case fontTrueTypeOT: {
             std::unique_ptr<FoFiTrueType> ff;
-            if (fileName) {
-                ff = FoFiTrueType::load(fileName->c_str());
+            if (!fileName.empty()) {
+                ff = FoFiTrueType::load(fileName.c_str());
             } else {
                 ff = FoFiTrueType::make(tmpBuf, tmpBufLen);
             }
@@ -2023,8 +2023,8 @@ reload:
                 }
             } else {
                 std::unique_ptr<FoFiTrueType> ff;
-                if (fileName) {
-                    ff = FoFiTrueType::load(fileName->c_str());
+                if (!fileName.empty()) {
+                    ff = FoFiTrueType::load(fileName.c_str());
                 } else {
                     ff = FoFiTrueType::make(tmpBuf, tmpBufLen);
                 }

splash/SplashFTFontFile.cc
@@ -42,7 +42,7 @@ SplashFontFile *SplashFTFontFile::loadType1Font(SplashFTFontEngine *engineA, Spl
     int i;
 
     if (src->isFile) {
-        if (FT_New_Face(engineA->lib, src->fileName->c_str(), 0, &faceA)) {
+        if (FT_New_Face(engineA->lib, src->fileName.c_str(), 0, &faceA)) {
             return nullptr;
         }
     } else {
@@ -72,7 +72,7 @@ SplashFontFile *SplashFTFontFile::loadCIDFont(SplashFTFontEngine *engineA, Splas
     FT_Face faceA;
 
     if (src->isFile) {
-        if (FT_New_Face(engineA->lib, src->fileName->c_str(), 0, &faceA)) {
+        if (FT_New_Face(engineA->lib, src->fileName.c_str(), 0, &faceA)) {
             return nullptr;
         }
     } else {
@@ -89,7 +89,7 @@ SplashFontFile *SplashFTFontFile::loadTrueTypeFont(SplashFTFontEngine *engineA, 
     FT_Face faceA;
 
     if (src->isFile) {
-        if (FT_New_Face(engineA->lib, src->fileName->c_str(), faceIndexA, &faceA)) {
+        if (FT_New_Face(engineA->lib, src->fileName.c_str(), faceIndexA, &faceA)) {
             return nullptr;
         }
     } else {

splash/SplashFontFile.h
@@ -23,10 +23,11 @@
 #ifndef SPLASHFONTFILE_H
 #define SPLASHFONTFILE_H
 
+#include <string>
+
 #include "SplashTypes.h"
 #include "poppler_private_export.h"
 
-class GooString;
 class SplashFontEngine;
 class SplashFont;
 class SplashFontFileID;
@@ -43,14 +44,14 @@ public:
     SplashFontSrc(const SplashFontSrc &) = delete;
     SplashFontSrc &operator=(const SplashFontSrc &) = delete;
 
-    void setFile(const GooString *file);
+    void setFile(const std::string &file);
     void setBuf(char *bufA, int buflenA);
 
     void ref();
     void unref();
 
     bool isFile;
-    GooString *fileName;
+    std::string fileName;
     char *buf;
     int bufLen;
 

commit 6551e1676265f8ae0b7aab4da87f25bc7e281bae

Fix crash caused by efe7d77838896ddcefb838f382628ae31be54d99

splash/SplashFontFile.cc
@@ -12,7 +12,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2006 Takashi Iwai <tiwai@suse.de>
-// Copyright (C) 2008 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2008, 2022 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2019 Christian Persch <chpe@src.gnome.org>
 // Copyright (C) 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 //
@@ -68,7 +68,6 @@ void SplashFontFile::decRefCnt()
 SplashFontSrc::SplashFontSrc()
 {
     isFile = false;
-    fileName = nullptr;
     buf = nullptr;
     refcnt = 1;
 }