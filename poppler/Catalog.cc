tsdgeos/poppler_mirror

commit 5f915d46c99ecbc0c026b86de50f9e0243391a01

Annotations: Make sure we embed fonts for the FreeText annots

poppler/Catalog.cc
@@ -103,7 +103,13 @@ Catalog::Catalog(PDFDoc *docA)
         return;
     }
     // get the AcroForm dictionary
-    acroForm = catDict.dictLookup("AcroForm");
+    acroForm = catDict.getDict()->lookup("AcroForm");
+    if (acroForm.isDict()) {
+        // We later assume the Fields Array exists, so check it does
+        if (!acroForm.dictLookup("Fields").isArray()) {
+            acroForm.setToNull();
+        }
+    }
 
     // read base URI
     Object obj = catDict.getDict()->lookupEnsureEncryptedIfNeeded("URI");
@@ -1055,6 +1061,26 @@ Catalog::FormType Catalog::getFormType()
     return res;
 }
 
+Form *Catalog::getCreateForm()
+{
+    catalogLocker();
+    if (!form) {
+        if (!acroForm.isDict()) {
+            acroForm = Object(new Dict(xref));
+            acroForm.dictSet("Fields", Object(new Array(xref)));
+
+            const Ref newFormRef = xref->addIndirectObject(acroForm);
+
+            Object catDict = xref->getCatalog();
+            catDict.dictSet("AcroForm", Object(newFormRef));
+
+            xref->setModifiedObject(&catDict, { xref->getRootNum(), xref->getRootGen() });
+        }
+    }
+
+    return getForm();
+}
+
 Form *Catalog::getForm()
 {
     catalogLocker();
@@ -1073,28 +1099,23 @@ void Catalog::addFormToAcroForm(const Ref formRef)
 {
     catalogLocker();
 
+    if (!acroForm.isDict()) {
+        getCreateForm();
+    }
+
+    // append to field array
+    Ref fieldRef;
+    Object fieldArray = acroForm.getDict()->lookup("Fields", &fieldRef);
+    fieldArray.getArray()->add(Object(formRef));
+
+    setAcroFormModified();
+}
+
+void Catalog::setAcroFormModified()
+{
     Object catDict = xref->getCatalog();
     Ref acroFormRef;
-    acroForm = catDict.getDict()->lookup("AcroForm", &acroFormRef);
-
-    if (!acroForm.isDict()) {
-        // none there yet, need to create a new fields dict
-        Object newForm = Object(new Dict(xref));
-        newForm.dictSet("SigFlags", Object(3));
-
-        Array *fieldArray = new Array(xref);
-        fieldArray->add(Object(formRef));
-        newForm.dictSet("Fields", Object(fieldArray));
-
-        Ref newRef = xref->addIndirectObject(newForm);
-        catDict.dictSet("AcroForm", Object(newRef));
-        acroForm = catDict.getDict()->lookup("AcroForm");
-    } else {
-        // append to field array
-        Ref fieldRef;
-        Object fieldArray = acroForm.getDict()->lookup("Fields", &fieldRef);
-        fieldArray.getArray()->add(Object(formRef));
-    }
+    catDict.getDict()->lookup("AcroForm", &acroFormRef);
 
     if (acroFormRef != Ref::INVALID()) {
         xref->setModifiedObject(&acroForm, acroFormRef);
@@ -1108,9 +1129,6 @@ void Catalog::removeFormFromAcroForm(const Ref formRef)
     catalogLocker();
 
     Object catDict = xref->getCatalog();
-    Ref acroFormRef;
-    acroForm = catDict.getDict()->lookup("AcroForm", &acroFormRef);
-
     if (acroForm.isDict()) {
         // remove from field array
         Ref fieldRef;
@@ -1124,7 +1142,7 @@ void Catalog::removeFormFromAcroForm(const Ref formRef)
             }
         }
 
-        xref->setModifiedObject(&acroForm, acroFormRef);
+        setAcroFormModified();
     }
 }
 

poppler/Annot.cc
@@ -1513,6 +1513,8 @@ void Annot::update(const char *key, Object &&value)
     annotObj.dictSet(const_cast<char *>(key), std::move(value));
 
     doc->getXRef()->setModifiedObject(&annotObj, ref);
+
+    hasBeenUpdated = true;
 }
 
 void Annot::setContents(std::unique_ptr<GooString> &&new_content)
@@ -2094,6 +2096,7 @@ void Annot::setNewAppearance(Object &&newAppearance)
         Object obj1 = Object(new Dict(doc->getXRef()));
         obj1.dictAdd("N", Object(updatedAppearanceStream));
         update("AP", std::move(obj1));
+        update("AS", Object(objName, "N"));
 
         Object updatedAP = annotObj.dictLookup("AP");
         appearStreams = std::make_unique<AnnotAppearance>(doc, &updatedAP);
@@ -2831,13 +2834,12 @@ void AnnotLink::draw(Gfx *gfx, bool printing)
 //------------------------------------------------------------------------
 const double AnnotFreeText::undefinedFontPtSize = 10.;
 
-AnnotFreeText::AnnotFreeText(PDFDoc *docA, PDFRectangle *rectA, const DefaultAppearance &da) : AnnotMarkup(docA, rectA)
+AnnotFreeText::AnnotFreeText(PDFDoc *docA, PDFRectangle *rectA) : AnnotMarkup(docA, rectA)
 {
     type = typeFreeText;
 
-    const std::string daStr = da.toAppearanceString();
     annotObj.dictSet("Subtype", Object(objName, "FreeText"));
-    annotObj.dictSet("DA", Object(new GooString(daStr)));
+    annotObj.dictSet("DA", Object(new GooString()));
 
     initialize(docA, annotObj.getDict());
 }
@@ -3041,6 +3043,64 @@ static std::unique_ptr<GfxFont> createAnnotDrawFont(XRef *xref, Dict *fontParent
     return GfxFont::makeFont(xref, resourceName, dummyRef, fontDict);
 }
 
+class HorizontalTextLayouter
+{
+public:
+    void add(const std::string &text, const std::string &fontName, const double width) { data.emplace_back(text, fontName, width); }
+
+    std::string layout(const VariableTextQuadding quadding, const double availableWidth, const double fontSize, double &xposPrev) const
+    {
+        double totalWidth = 0;
+        for (const Data &d : data) {
+            totalWidth += d.width;
+        }
+
+        double newXpos;
+        switch (quadding) {
+        case VariableTextQuadding::centered:
+            newXpos = (availableWidth - totalWidth) / 2;
+            break;
+        case VariableTextQuadding::rightJustified:
+            newXpos = availableWidth - totalWidth;
+            break;
+        default: // VariableTextQuadding::lLeftJustified:
+            newXpos = 0;
+            break;
+        }
+
+        AnnotAppearanceBuilder builder;
+        bool first = true;
+        double prevBlockWidth = 0;
+        for (const Data &d : data) {
+            builder.appendf("/{0:s} {1:.2f} Tf\n", d.fontName.c_str(), fontSize);
+
+            const double yDiff = first ? -fontSize : 0;
+            const double xDiff = first ? newXpos - xposPrev : prevBlockWidth;
+
+            builder.appendf("{0:.2f} {1:.2f} Td\n", xDiff, yDiff);
+            builder.writeString(d.text);
+            builder.append("Tj\n");
+            first = false;
+            prevBlockWidth = d.width;
+        }
+        xposPrev = newXpos + totalWidth - prevBlockWidth;
+
+        return builder.buffer()->toStr();
+    }
+
+private:
+    struct Data
+    {
+        Data(const std::string &t, const std::string &fName, double w) : text(t), fontName(fName), width(w) { }
+
+        const std::string text;
+        const std::string fontName;
+        const double width;
+    };
+
+    std::vector<Data> data;
+};
+
 void AnnotFreeText::generateFreeTextAppearance()
 {
     double borderWidth, ca = opacity;
@@ -3132,30 +3192,78 @@ void AnnotFreeText::generateFreeTextAppearance()
     // Set font state
     appearBuilder.setDrawColor(da.getFontColor(), true);
     appearBuilder.appendf("BT 1 0 0 1 {0:.2f} {1:.2f} Tm\n", textmargin, height - textmargin - da.getFontPtSize() * font->getDescent());
-    appearBuilder.setTextFont(da.getFontName(), da.getFontPtSize());
 
     int i = 0;
     double xposPrev = 0;
+    const bool isUnicode = contents->hasUnicodeMarker();
     while (i < contents->getLength()) {
+        HorizontalTextLayouter textLayouter;
+
         GooString out;
-        double linewidth, xpos;
-        layoutText(contents.get(), &out, &i, *font, &linewidth, textwidth / da.getFontPtSize(), nullptr, false);
-        linewidth *= da.getFontPtSize();
-        switch (quadding) {
-        case VariableTextQuadding::centered:
-            xpos = (textwidth - linewidth) / 2;
-            break;
-        case VariableTextQuadding::rightJustified:
-            xpos = textwidth - linewidth;
-            break;
-        default: // VariableTextQuadding::leftJustified:
-            xpos = 0;
-            break;
+        double availableTextWidthInFontPtSize = textwidth / da.getFontPtSize();
+        double blockWidth;
+        bool newFontNeeded;
+        layoutText(contents.get(), &out, &i, *font, &blockWidth, availableTextWidthInFontPtSize, nullptr, false, &newFontNeeded);
+        availableTextWidthInFontPtSize -= blockWidth;
+        textLayouter.add(out.toStr(), da.getFontName().getName(), blockWidth * da.getFontPtSize());
+
+        while (availableTextWidthInFontPtSize >= 0 && newFontNeeded) {
+            if (!form) {
+                // There's no fonts to look for, so just skip the characters
+                i += isUnicode ? 2 : 1;
+                error(errSyntaxError, -1, "AnnotFreeText::generateFreeTextAppearance, found character that the font can't represent");
+                newFontNeeded = false;
+            } else {
+                Unicode uChar;
+                if (isUnicode) {
+                    uChar = (unsigned char)(contents->getChar(i)) << 8;
+                    uChar += (unsigned char)(contents->getChar(i + 1));
+                } else {
+                    uChar = pdfDocEncoding[contents->getChar(i) & 0xff];
+                }
+                const std::string auxFontName = form->getFallbackFontForChar(uChar, *font);
+                if (!auxFontName.empty()) {
+                    std::shared_ptr<GfxFont> auxFont = form->getDefaultResources()->lookupFont(auxFontName.c_str());
+
+                    // Here we just layout one char, we don't know if the one afterwards can be layouted with the original font
+                    GooString auxContents = GooString(contents->toStr().substr(i, isUnicode ? 2 : 1));
+                    if (isUnicode) {
+                        auxContents.prependUnicodeMarker();
+                    }
+                    int auxI = 0;
+                    layoutText(&auxContents, &out, &auxI, *auxFont, &blockWidth, availableTextWidthInFontPtSize, nullptr, false, &newFontNeeded);
+                    assert(!newFontNeeded);
+                    // layoutText will always at least layout one character even if it doesn't fit in
+                    // the given space which makes sense (except in the case of switching fonts, so we control if we ran out of space here manually)
+                    availableTextWidthInFontPtSize -= blockWidth;
+                    if (availableTextWidthInFontPtSize >= 0) {
+                        i += auxI - (isUnicode ? 2 : 0); // the second term is the unicode marker
+                        textLayouter.add(out.toStr(), auxFontName, blockWidth * da.getFontPtSize());
+                    }
+
+                } else {
+                    error(errSyntaxError, -1, "AnnotFreeText::generateFreeTextAppearance, couldn't find a font for character U+{0:04uX}", uChar);
+                    newFontNeeded = false;
+                    i += contents->hasUnicodeMarker() ? 2 : 1;
+                }
+            }
+
+            // Now layout the rest of the text with the original font if we still have space
+            if (availableTextWidthInFontPtSize >= 0) {
+                layoutText(contents.get(), &out, &i, *font, &blockWidth, availableTextWidthInFontPtSize, nullptr, false, &newFontNeeded);
+                availableTextWidthInFontPtSize -= blockWidth;
+                // layoutText will always at least layout one character even if it doesn't fit in
+                // the given space which makes sense (except in the case of switching fonts, so we control if we ran out of space here manually)
+                if (availableTextWidthInFontPtSize >= 0) {
+                    textLayouter.add(out.toStr(), da.getFontName().getName(), blockWidth * da.getFontPtSize());
+                } else {
+                    i -= isUnicode ? 2 : 1;
+                }
+            }
         }
-        appearBuilder.appendf("{0:.2f} {1:.2f} Td\n", xpos - xposPrev, -da.getFontPtSize());
-        appearBuilder.writeString(out.toStr());
-        appearBuilder.append("Tj\n");
-        xposPrev = xpos;
+
+        const std::string layoutedLine = textLayouter.layout(quadding, textwidth, da.getFontPtSize(), xposPrev);
+        appearBuilder.append(layoutedLine.c_str());
     }
 
     appearBuilder.append("ET Q\n");
@@ -3165,14 +3273,23 @@ void AnnotFreeText::generateFreeTextAppearance()
     bbox[2] = rect->x2 - rect->x1;
     bbox[3] = rect->y2 - rect->y1;
 
+    Object newAppearance;
     if (ca == 1) {
-        appearance = createForm(appearBuilder.buffer(), bbox, false, std::move(resourceObj));
+        newAppearance = createForm(appearBuilder.buffer(), bbox, false, std::move(resourceObj));
     } else {
         Object aStream = createForm(appearBuilder.buffer(), bbox, true, std::move(resourceObj));
 
         GooString appearBuf("/GS0 gs\n/Fm0 Do");
         Dict *resDict = createResourcesDict("Fm0", std::move(aStream), "GS0", ca, nullptr);
-        appearance = createForm(&appearBuf, bbox, false, resDict);
+        newAppearance = createForm(&appearBuf, bbox, false, resDict);
+    }
+    if (hasBeenUpdated) {
+        // We should technically do this for all annots but AnnotFreeText
+        // is particularly special since we're potentially embeddeing a font so we really need
+        // to set the AP and not let other renderers guess it from the contents
+        setNewAppearance(std::move(newAppearance));
+    } else {
+        appearance = std::move(newAppearance);
     }
 }
 
@@ -4078,7 +4195,7 @@ bool AnnotWidget::setFormAdditionalAction(FormAdditionalActionsType formAddition
 // TODO: Handle surrogate pairs in UTF-16.
 //       Should be able to generate output for any CID-keyed font.
 //       Doesn't handle vertical fonts--should it?
-void Annot::layoutText(const GooString *text, GooString *outBuf, int *i, const GfxFont &font, double *width, double widthLimit, int *charCount, bool noReencode)
+void Annot::layoutText(const GooString *text, GooString *outBuf, int *i, const GfxFont &font, double *width, double widthLimit, int *charCount, bool noReencode, bool *newFontNeeded)
 {
     CharCode c;
     Unicode uChar;
@@ -4087,6 +4204,9 @@ void Annot::layoutText(const GooString *text, GooString *outBuf, int *i, const G
     int uLen, n;
     double dx, dy, ox, oy;
 
+    if (newFontNeeded) {
+        *newFontNeeded = false;
+    }
     if (width != nullptr) {
         *width = 0.0;
     }
@@ -4171,10 +4291,28 @@ void Annot::layoutText(const GooString *text, GooString *outBuf, int *i, const G
                 outBuf->append(uChar & 0xff);
             } else if (ccToUnicode->mapToCharCode(&uChar, &c, 1)) {
                 if (font.isCIDFont()) {
-                    // TODO: This assumes an identity CMap.  It should be extended to
-                    // handle the general case.
-                    outBuf->append((c >> 8) & 0xff);
-                    outBuf->append(c & 0xff);
+                    auto cidFont = static_cast<const GfxCIDFont *>(&font);
+                    if (c < cidFont->getCIDToGIDLen()) {
+                        const int glyph = cidFont->getCIDToGID()[c];
+                        if (glyph > 0 || c == 0) {
+                            outBuf->append((c >> 8) & 0xff);
+                            outBuf->append(c & 0xff);
+                        } else {
+                            if (newFontNeeded) {
+                                *newFontNeeded = true;
+                                *i -= unicode ? 2 : 1;
+                                break;
+                            }
+                            outBuf->append((c >> 8) & 0xff);
+                            outBuf->append(c & 0xff);
+                            error(errSyntaxError, -1, "AnnotWidget::layoutText, font doesn't have glyph for charcode U+{0:04uX}", c);
+                        }
+                    } else {
+                        // TODO: This assumes an identity CMap.  It should be extended to
+                        // handle the general case.
+                        outBuf->append((c >> 8) & 0xff);
+                        outBuf->append(c & 0xff);
+                    }
                 } else {
                     // 8-bit font
                     outBuf->append(c);

poppler/Annot.h
@@ -772,7 +772,8 @@ public:
     // Check if point is inside the annot rectangle.
     bool inRect(double x, double y) const;
 
-    static void layoutText(const GooString *text, GooString *outBuf, int *i, const GfxFont &font, double *width, double widthLimit, int *charCount, bool noReencode);
+    // If newFontNeeded is not null, it will contain whether the given font has glyphs to represent the needed text
+    static void layoutText(const GooString *text, GooString *outBuf, int *i, const GfxFont &font, double *width, double widthLimit, int *charCount, bool noReencode, bool *newFontNeeded = nullptr);
 
 private:
     void readArrayNum(Object *pdfArray, int key, double *value);
@@ -827,6 +828,8 @@ protected:
 
     bool hasRef;
     mutable std::recursive_mutex mutex;
+
+    bool hasBeenUpdated = false;
 };
 
 //------------------------------------------------------------------------
@@ -1055,7 +1058,7 @@ public:
 
     static const double undefinedFontPtSize;
 
-    AnnotFreeText(PDFDoc *docA, PDFRectangle *rect, const DefaultAppearance &da);
+    AnnotFreeText(PDFDoc *docA, PDFRectangle *rect);
     AnnotFreeText(PDFDoc *docA, Object &&dictObject, const Object *obj);
     ~AnnotFreeText() override;
 

poppler/Catalog.h
@@ -212,6 +212,7 @@ public:
     Object *getAcroForm() { return &acroForm; }
     void addFormToAcroForm(const Ref formRef);
     void removeFormFromAcroForm(const Ref formRef);
+    void setAcroFormModified();
 
     OCGs *getOptContentConfig() { return optContent; }
 
@@ -226,6 +227,7 @@ public:
     };
 
     FormType getFormType();
+    Form *getCreateForm();
     Form *getForm();
 
     ViewerPreferences *getViewerPreferences();

poppler/Dict.cc
@@ -247,3 +247,13 @@ bool Dict::hasKey(const char *key) const
 {
     return find(key) != nullptr;
 }
+
+std::string Dict::findAvailableKey(const std::string &suggestedKey)
+{
+    int i = 0;
+    std::string res = suggestedKey;
+    while (find(res.c_str())) {
+        res = suggestedKey + std::to_string(i++);
+    }
+    return res;
+}

poppler/Dict.h
@@ -16,7 +16,7 @@
 // Copyright (C) 2005 Kristian Høgsberg <krh@redhat.com>
 // Copyright (C) 2006 Krzysztof Kowalczyk <kkowalczyk@gmail.com>
 // Copyright (C) 2007-2008 Julien Rebetez <julienr@svn.gnome.org>
-// Copyright (C) 2010, 2017-2021 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2010, 2017-2022 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2010 Paweł Wiejacha <pawel.wiejacha@gmail.com>
 // Copyright (C) 2013 Thomas Freitag <Thomas.Freitag@alfa.de>
 // Copyright (C) 2017 Adrian Johnson <ajohnson@redneon.com>
@@ -102,6 +102,11 @@ public:
     XRef *getXRef() const { return xref; }
 
     bool hasKey(const char *key) const;
+
+    // Returns a key name that is not in the dictionary
+    // It will be suggestedKey itself if available
+    // otherwise it will start adding 0, 1, 2, 3, etc. to suggestedKey until there's one available
+    std::string findAvailableKey(const std::string &suggestedKey);
 
 private:
     friend class Object; // for incRef/decRef

commit ac9b736c3f20704a41db9102589fa259abb26891

Fix rendering regression introduced in 5f915d46c99ecbc0c026b86de50f9e0243391a01
We can't assume "Fields" needs to exist, there's other things there like
NeedAppearances that are valid without Fields

The bug in Catalog::addFormToAcroForm remains, but will need to wait to
get fixed on next release

poppler/Catalog.cc
@@ -104,12 +104,6 @@ Catalog::Catalog(PDFDoc *docA)
     }
     // get the AcroForm dictionary
     acroForm = catDict.getDict()->lookup("AcroForm");
-    if (acroForm.isDict()) {
-        // We later assume the Fields Array exists, so check it does
-        if (!acroForm.dictLookup("Fields").isArray()) {
-            acroForm.setToNull();
-        }
-    }
 
     // read base URI
     Object obj = catDict.getDict()->lookupEnsureEncryptedIfNeeded("URI");