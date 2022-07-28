b443f529a1a35b9d5028cb95c6dcc0111e1acec1
Albert Astals Cid <aacid@kde.org>
2022-07-29 00:25:33 +0200

    Update (C)

--- poppler/CairoFontEngine.cc
+++ poppler/CairoFontEngine.cc
@@ -32,6 +32,7 @@
 // Copyright (C) 2019 Christian Persch <chpe@src.gnome.org>
 // Copyright (C) 2020 Michal <sudolskym@gmail.com>
 // Copyright (C) 2021, 2022 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2022 Marcel Fabian Krüger <tex@2krueger.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
