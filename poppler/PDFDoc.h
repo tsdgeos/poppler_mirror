c569e5d819de1e53813ebd8409e57c297d069537
Albert Astals Cid <aacid@kde.org>
2022-08-13 18:55:57 +0200

    Add (C)

--- poppler/PDFDoc.h
+++ poppler/PDFDoc.h
@@ -38,6 +38,7 @@
 // Copyright (C) 2021 Georgiy Sgibnev <georgiy@sgibnev.com>. Work sponsored by lab50.net.
 // Copyright (C) 2021 Marek Kasik <mkasik@redhat.com>
 // Copyright (C) 2022 Felix Jung <fxjung@posteo.de>
+// Copyright (C) 2022 crt <chluo@cse.cuhk.edu.hk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
