2cd23e7e6de1e854e22c7fd666a9167a58662211
Albert Astals Cid <aacid@kde.org>
2022-09-07 01:03:57 +0200

    Update (C)

--- utils/pdftoppm.cc
+++ utils/pdftoppm.cc
@@ -37,6 +37,7 @@
 // Copyright (C) 2020 Philipp Knechtges <philipp-dev@knechtges.com>
 // Copyright (C) 2021 Diogo Kollross <diogoko@gmail.com>
 // Copyright (C) 2021 Peter Williams <peter@newton.cx>
+// Copyright (C) 2022 James Cloos <cloos@jhcloos.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
