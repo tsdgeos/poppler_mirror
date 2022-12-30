43e2da4ade60f7e6c62b4d70f15d603327ca7b2b
Albert Astals Cid <aacid@kde.org>
2022-12-30 23:49:15 +0100

    Update (C)

--- poppler/CairoOutputDev.cc
+++ poppler/CairoOutputDev.cc
@@ -36,6 +36,7 @@
 // Copyright (C) 2020, 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2021 Uli Schlachter <psychon@znc.in>
 // Copyright (C) 2021 Christian Persch <chpe@src.gnome.org>
+// Copyright (C) 2022 Zachary Travis <ztravis@everlaw.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
