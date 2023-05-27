c6dd11d89a08a6e4ab93d31b6d5d89a9153d9a91
Albert Astals Cid <aacid@kde.org>
2023-05-28 01:00:51 +0200

    Update (C)

--- poppler/GfxFont.cc
+++ poppler/GfxFont.cc
@@ -36,6 +36,7 @@
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2019 LE GARREC Vincent <legarrec.vincent@gmail.com>
 // Copyright (C) 2021, 2022 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2023 Khaled Hosny <khaled@aliftype.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
