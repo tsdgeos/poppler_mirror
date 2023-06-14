7b199526e6f7bb5a28e8ddd017e23ae51b85b9f6
Sune Vuorela <sune@vuorela.dk>
2023-06-14 16:13:23 +0200

    Cryptosign: OCSP and AIA: Document
    Document how the backends interacts with the OCSP and AIA flags,
    and ensure the implementation matches.

--- poppler/GPGMECryptoSignBackend.cc
+++ poppler/GPGMECryptoSignBackend.cc
@@ -345,7 +345,7 @@ CertificateValidationStatus GpgSignatureVerification::validateCertificate(std::c
         return CERTIFICATE_GENERIC_ERROR;
     }
     const auto offline = gpgContext->offline();
-    gpgContext->setOffline(!ocspRevocationCheck);
+    gpgContext->setOffline((!ocspRevocationCheck) || useAIACertFetch);
     const auto key = signature->key(true, true);
     gpgContext->setOffline(offline);
     if (key.isExpired()) {
