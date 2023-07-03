1b1036cf4ef1bfd70f300da3534375d02195fe32
Albert Astals Cid <aacid@kde.org>
2023-07-03 21:05:14 +0200

    Update (C)

--- poppler/Gfx.cc
+++ poppler/Gfx.cc
@@ -46,6 +46,7 @@
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
 // Copyright (C) 2020 Philipp Knechtges <philipp-dev@knechtges.com>
 // Copyright (C) 2021 Steve Rosenhamer <srosenhamer@me.com>
+// Copyright (C) 2023 Anton Thomasson <antonthomasson@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
