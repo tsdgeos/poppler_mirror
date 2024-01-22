6a4c667ee4096ba06e46d86bfc2b18a67e346303
Albert Astals Cid <aacid@kde.org>
2024-01-23 00:02:05 +0100

    Update (C)

--- poppler/TextOutputDev.cc
+++ poppler/TextOutputDev.cc
@@ -44,6 +44,7 @@
 // Copyright (C) 2019, 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2019 Dan Shea <dan.shea@logical-innovations.com>
 // Copyright (C) 2021 Peter Williams <peter@newton.cx>
+// Copyright (C) 2024 Adam Sampson <ats@offog.org>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
