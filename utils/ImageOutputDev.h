7c9dfc10fd7b3ee3bdd1a71ab932ffd93dfebeba
Albert Astals Cid <aacid@kde.org>
2024-01-25 00:13:22 +0100

    Update (C)

--- utils/ImageOutputDev.h
+++ utils/ImageOutputDev.h
@@ -20,6 +20,7 @@
 // Copyright (C) 2012, 2013, 2017 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2013 Thomas Freitag <Thomas.Freitag@alfa.de>
 // Copyright (C) 2018, 2019, 2021 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2024 Fernando Herrera <fherrera@onirica.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
--- utils/pdfimages.cc
+++ utils/pdfimages.cc
@@ -23,6 +23,7 @@
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2019, 2021 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2019 Hartmut Goebel <h.goebel@crazy-compilers.com>
+// Copyright (C) 2024 Fernando Herrera <fherrera@onirica.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
