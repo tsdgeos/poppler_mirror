2b2d5bc0a7351895c662958105dac11adfecf794
Albert Astals Cid <aacid@kde.org>
2024-01-25 23:49:11 +0100

    Update (C)

--- poppler/JBIG2Stream.cc
+++ poppler/JBIG2Stream.cc
@@ -28,6 +28,7 @@
 // Copyright (C) 2019-2021 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
 // Copyright (C) 2019-2021 Even Rouault <even.rouault@spatialys.com>
+// Copyright (C) 2014 Nelson Benítez León <nbenitezl@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
