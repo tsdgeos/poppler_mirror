0ad01ca51e6b6e061ba85db5b906dfc299dba8e1
Albert Astals Cid <aacid@kde.org>
2024-02-22 09:19:57 +0100

    Update (C)

--- poppler/XRef.cc
+++ poppler/XRef.cc
@@ -35,6 +35,7 @@
 // Copyright (C) 2023 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 // Copyright (C) 2023 Ilaï Deutel <idtl@google.com>
 // Copyright (C) 2023 Even Rouault <even.rouault@spatialys.com>
+// Copyright (C) 2024 Nelson Benítez León <nbenitezl@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
