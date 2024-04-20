5aa5d29c32b404cdc4c8d1a67b3b1bc8f068976d
Albert Astals Cid <aacid@kde.org>
2024-04-20 16:46:55 +0200

    Update (C)

--- poppler/UTF.h
+++ poppler/UTF.h
@@ -12,7 +12,7 @@
 // Copyright (C) 2021 Georgiy Sgibnev <georgiy@sgibnev.com>. Work sponsored by lab50.net.
 // Copyright (C) 2023, 2024 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 // Copyright (C) 2023 Even Rouault <even.rouault@spatialys.com>
-// Copyright (C) 2023 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2023, 2024 Oliver Sander <oliver.sander@tu-dresden.de>
 //
 //========================================================================
 
