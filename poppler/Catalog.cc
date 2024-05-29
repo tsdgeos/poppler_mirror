2fb40d2db82e0650500d76b61a0fae47b49d4193
Albert Astals Cid <aacid@kde.org>
2024-05-29 19:19:28 +0200

    Update (C)

--- poppler/Catalog.cc
+++ poppler/Catalog.cc
@@ -41,6 +41,7 @@
 // Copyright (C) 2020 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by Technische Universität Dresden
 // Copyright (C) 2021 RM <rm+git@arcsin.org>
 // Copyright (C) 2023 Ilaï Deutel <idtl@google.com>
+// Copyright (C) 2024 Hubert Figuiere <hub@figuiere.net>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
