cde04f579055729143ba74c3551127ce2499d8b7
Albert Astals Cid <aacid@kde.org>
2024-10-11 18:15:41 +0200

    Update (C)

--- poppler/Link.h
+++ poppler/Link.h
@@ -23,6 +23,7 @@
 // Copyright (C) 2019, 2020 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2020 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2020 Marek Kasik <mkasik@redhat.com>
+// Copyright (C) 2024 Pratham Gandhi <ppg.1382@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
