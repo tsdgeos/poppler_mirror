153c68fe87f1181ce5262a612774260d1d9cb3e0
Albert Astals Cid <aacid@kde.org>
2024-10-20 23:27:42 +0200

    Update (C)

--- poppler/Annot.cc
+++ poppler/Annot.cc
@@ -57,6 +57,7 @@
 // Copyright (C) 2022, 2024 Erich E. Hoover <erich.e.hoover@gmail.com>
 // Copyright (C) 2023 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 // Copyright (C) 2024 Pratham Gandhi <ppg.1382@gmail.com>
+// Copyright (C) 2024 Carsten Emde <ce@ceek.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
--- poppler/Annot.h
+++ poppler/Annot.h
@@ -43,6 +43,7 @@
 // Copyright (C) 2021 Georgiy Sgibnev <georgiy@sgibnev.com>. Work sponsored by lab50.net.
 // Copyright (C) 2022 Martin <martinbts@gmx.net>
 // Copyright (C) 2024 Erich E. Hoover <erich.e.hoover@gmail.com>
+// Copyright (C) 2024 Carsten Emde <ce@ceek.de>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
