5ad31b4b5532b7e68f5e0584290546c457b31111
Albert Astals Cid <aacid@kde.org>
2024-11-02 22:47:04 +0100

    Update (C)

--- splash/SplashFontFile.h
+++ splash/SplashFontFile.h
@@ -14,6 +14,7 @@
 // Copyright (C) 2006 Takashi Iwai <tiwai@suse.de>
 // Copyright (C) 2008, 2010, 2018 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2022 Oliver Sander <oliver.sander@tu-dresden.de>
+// Copyright (C) 2024 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
