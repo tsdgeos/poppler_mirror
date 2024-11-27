945a32731327ae2547753c6802aa51f932d129af
Albert Astals Cid <aacid@kde.org>
2024-11-28 00:01:35 +0100

    Update (C)

--- poppler/Parser.cc
+++ poppler/Parser.cc
@@ -22,6 +22,7 @@
 // Copyright (C) 2018 Klarälvdalens Datakonsult AB, a KDAB Group company, <info@kdab.com>. Work sponsored by the LiMux project of the city of Munich
 // Copyright (C) 2018, 2019 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2018 Marek Kasik <mkasik@redhat.com>
+// Copyright (C) 2024 Nelson Benítez León <nbenitezl@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
