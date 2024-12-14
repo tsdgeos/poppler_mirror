2992cc937accb1844a185a77824784f05ba38ed8
Albert Astals Cid <aacid@kde.org>
2024-12-14 10:30:06 +0100

    Update (C)

--- poppler/UnicodeMap.cc
+++ poppler/UnicodeMap.cc
@@ -20,6 +20,7 @@
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2019 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
+// Copyright (C) 2024 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
--- poppler/UnicodeMap.h
+++ poppler/UnicodeMap.h
@@ -19,6 +19,7 @@
 // Copyright (C) 2018-2022 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
+// Copyright (C) 2024 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
