a3fd179de9002c26da5cac62ca728894808e6513
Albert Astals Cid <aacid@kde.org>
2024-12-20 12:32:40 +0100

    Update (C)

--- poppler/NameToCharCode.h
+++ poppler/NameToCharCode.h
@@ -14,6 +14,7 @@
 // under GPL version 2 or later
 //
 // Copyright (C) 2018, 2019 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2024 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
