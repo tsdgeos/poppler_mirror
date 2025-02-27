nvoronetskiy/poppler

Put a pgp signature in a pdf file
Allow putting a pgp signature in a pdf file.
This is not fully documented inside the specification, but basically it
is replacing the CMS bundle as described with a pgp bundle.

These signatures are tagged in the g10c namespace
(https://github.com/adobe/pdf-names-list/issues/45) and is only
implemented in the GPGME backend as a 'whatever gpg understands'.

A special difference is that the signature is not padded with zeroes,
but with a gpg comment packet with the exact size to fill the void.

qt5/src/poppler-form.cc
@@ -52,6 +52,9 @@
 #ifdef ENABLE_NSS3
 #    include <NSSCryptoSignBackend.h>
 #endif
+#ifdef ENABLE_GPGME
+#    include <GPGMECryptoSignBackend.h>
+#endif
 
 #include "poppler-page-private.h"
 #include "poppler-private.h"
@@ -617,6 +620,7 @@ public:
     bool is_self_signed;
     bool is_null;
     CertificateInfo::KeyLocation keyLocation;
+    CertificateInfo::CertificateType certificateType;
 };
 
 CertificateInfo::CertificateInfo() : d_ptr(new CertificateInfoPrivate())
@@ -655,6 +659,12 @@ QByteArray CertificateInfo::serialNumber() const
 {
     Q_D(const CertificateInfo);
     return d->serial_number;
+}
+
+CertificateInfo::CertificateType CertificateInfo::certificateType() const
+{
+    Q_D(const CertificateInfo);
+    return d->certificateType;
 }
 
 QString CertificateInfo::issuerInfo(EntityInfoKey key) const
@@ -975,6 +985,9 @@ FormFieldSignature::SignatureType FormFieldSignature::signatureType() const
     case CryptoSign::SignatureType::unsigned_signature_field:
         sigType = UnsignedSignature;
         break;
+    case CryptoSign::SignatureType::g10c_pgp_signature_detached:
+        sigType = G10cPgpSignatureDetached;
+        break;
     }
     return sigType;
 }
@@ -1001,6 +1014,17 @@ static CertificateInfo::KeyLocation fromPopplerCore(KeyLocation location)
     return CertificateInfo::KeyLocation::Unknown;
 }
 
+static CertificateInfo::CertificateType fromPopplerCore(CertificateType type)
+{
+    switch (type) {
+    case CertificateType::PGP:
+        return CertificateInfo::CertificateType::PGP;
+    case CertificateType::X509:
+        return CertificateInfo::CertificateType::X509;
+    }
+    return CertificateInfo::CertificateType::X509; // fallback
+}
+
 static CertificateInfoPrivate *createCertificateInfoPrivate(const X509CertificateInfo *ci)
 {
     CertificateInfoPrivate *certPriv = new CertificateInfoPrivate;
@@ -1009,6 +1033,7 @@ static CertificateInfoPrivate *createCertificateInfoPrivate(const X509Certificat
         certPriv->version = ci->getVersion();
         certPriv->ku_extensions = ci->getKeyUsageExtensions();
         certPriv->keyLocation = fromPopplerCore(ci->getKeyLocation());
+        certPriv->certificateType = fromPopplerCore(ci->getCertificateType());
 
         const GooString &certSerial = ci->getSerialNumber();
         certPriv->serial_number = QByteArray(certSerial.c_str(), certSerial.getLength());
@@ -1342,4 +1367,22 @@ void setNSSPasswordCallback(const std::function<char *(const char *)> &f)
 #endif
 }
 
+void setPgpSignaturesAllowed(bool allowed)
+{
+#ifdef ENABLE_GPGME
+    GpgSignatureConfiguration::setPgpSignaturesAllowed(allowed);
+#else
+    qWarning() << "Trying to enable pgp signatures, but pgp not enabled in this build";
+    (void)allowed;
+#endif
+}
+
+bool isPgpSignaturesAllowed()
+{
+#ifdef ENABLE_GPGME
+    return GpgSignatureConfiguration::isPgpSignaturesAllowed();
+#else
+    return false;
+#endif
+}
 }

qt5/src/poppler-form.h
@@ -534,6 +534,17 @@ public:
     Q_DECLARE_FLAGS(KeyUsageExtensions, KeyUsageExtension)
 
     /**
+     * The type of the certificate
+     *
+     * \since 25.03
+     */
+    enum class CertificateType
+    {
+        X509,
+        PGP
+    };
+
+    /**
       Predefined keys for elements in an entity's distinguished name.
      */
     enum EntityInfoKey
@@ -632,6 +643,13 @@ public:
       Returns true if certificate is self-signed otherwise returns false.
      */
     bool isSelfSigned() const;
+
+    /**
+     * \return certificate type
+     *
+     * \since 25.03
+     */
+    CertificateType certificateType() const;
 
     /**
       The DER encoded certificate.
@@ -839,7 +857,8 @@ public:
         AdbePkcs7detached,
         EtsiCAdESdetached,
         UnknownSignatureType, ///< \since 0.90
-        UnsignedSignature ///< \since 22.02
+        UnsignedSignature, ///< \since 22.02
+        G10cPgpSignatureDetached, ///< non standard signature type \since 25.01
     };
 
     /**
@@ -1035,6 +1054,23 @@ void POPPLER_QT5_EXPORT setNSSDir(const QString &pathURL);
   \since 21.01
 */
 void POPPLER_QT5_EXPORT setNSSPasswordCallback(const std::function<char *(const char *)> &f);
+
+/**
+ * Allow pgp signatures in pdf files (standard-extension, experimental)
+ * Not supported for NSS backend
+ *
+ * \param allowed new value for pgp signatures allowed
+ *
+ * \since 25.03
+ */
+void POPPLER_QT5_EXPORT setPgpSignaturesAllowed(bool allowed);
+
+/**
+ * \returns if pgp signatures are allowed.
+ *
+ * \since 25.03
+ */
+bool POPPLER_QT5_EXPORT isPgpSignaturesAllowed();
 }
 
 #endif

qt6/src/poppler-form.cc
@@ -52,6 +52,9 @@
 #ifdef ENABLE_NSS3
 #    include <NSSCryptoSignBackend.h>
 #endif
+#ifdef ENABLE_GPGME
+#    include <GPGMECryptoSignBackend.h>
+#endif
 
 #include "poppler-page-private.h"
 #include "poppler-private.h"
@@ -617,6 +620,7 @@ public:
     bool is_self_signed;
     bool is_null;
     bool is_qualified;
+    CertificateInfo::CertificateType certificateType;
     CertificateInfo::KeyLocation keyLocation;
 };
 
@@ -662,6 +666,12 @@ bool CertificateInfo::isQualified() const
 {
     Q_D(const CertificateInfo);
     return d->is_qualified;
+}
+
+CertificateInfo::CertificateType CertificateInfo::certificateType() const
+{
+    Q_D(const CertificateInfo);
+    return d->certificateType;
 }
 
 QString CertificateInfo::issuerInfo(EntityInfoKey key) const
@@ -979,6 +989,9 @@ FormFieldSignature::SignatureType FormFieldSignature::signatureType() const
     case CryptoSign::SignatureType::unknown_signature_type:
         sigType = UnknownSignatureType;
         break;
+    case CryptoSign::SignatureType::g10c_pgp_signature_detached:
+        sigType = G10cPgpSignatureDetached;
+        break;
     case CryptoSign::SignatureType::unsigned_signature_field:
         sigType = UnsignedSignature;
         break;
@@ -991,6 +1004,17 @@ SignatureValidationInfo FormFieldSignature::validate(ValidateOptions opt) const
     auto tempResult = validateAsync(opt);
     tempResult.first.d_ptr->certificate_status = validateResult();
     return tempResult.first;
+}
+
+static CertificateInfo::CertificateType fromPopplerCore(CertificateType type)
+{
+    switch (type) {
+    case CertificateType::PGP:
+        return CertificateInfo::CertificateType::PGP;
+    case CertificateType::X509:
+        return CertificateInfo::CertificateType::X509;
+    }
+    return CertificateInfo::CertificateType::X509; // fallback
 }
 
 static CertificateInfo::KeyLocation fromPopplerCore(KeyLocation location)
@@ -1016,6 +1040,7 @@ static CertificateInfoPrivate *createCertificateInfoPrivate(const X509Certificat
         certPriv->version = ci->getVersion();
         certPriv->ku_extensions = ci->getKeyUsageExtensions();
         certPriv->keyLocation = fromPopplerCore(ci->getKeyLocation());
+        certPriv->certificateType = fromPopplerCore(ci->getCertificateType());
 
         const GooString &certSerial = ci->getSerialNumber();
         certPriv->serial_number = QByteArray(certSerial.c_str(), certSerial.getLength());
@@ -1360,6 +1385,25 @@ void setNSSPasswordCallback(const std::function<char *(const char *)> &f)
 #else
     qWarning() << "setNSSPasswordCallback called but this poppler is built without NSS support";
     (void)f;
+#endif
+}
+
+void setPgpSignaturesAllowed(bool allowed)
+{
+#ifdef ENABLE_GPGME
+    GpgSignatureConfiguration::setPgpSignaturesAllowed(allowed);
+#else
+    qWarning() << "Trying to enable pgp signatures, but pgp not enabled in this build";
+    (void)allowed;
+#endif
+}
+
+bool isPgpSignaturesAllowed()
+{
+#ifdef ENABLE_GPGME
+    return GpgSignatureConfiguration::isPgpSignaturesAllowed();
+#else
+    return false;
 #endif
 }
 }

qt6/src/poppler-form.h
@@ -510,6 +510,17 @@ public:
         Organization,
     };
 
+    /**
+     * The type of the certificate
+     *
+     * \since 25.03
+     */
+    enum class CertificateType
+    {
+        X509,
+        PGP
+    };
+
     /** A signing key can be located in different places
      sometimes. For the user, it might be easier to pick
      the key located on a card if it has some visual
@@ -605,6 +616,13 @@ public:
      * https://en.wikipedia.org/wiki/Qualified_electronic_signature
      */
     bool isQualified() const;
+
+    /**
+     * \return certificate type
+     *
+     * \since 25.03
+     */
+    CertificateType certificateType() const;
 
     /**
       The DER encoded certificate.
@@ -798,7 +816,8 @@ public:
         AdbePkcs7sha1,
         AdbePkcs7detached,
         EtsiCAdESdetached,
-        UnsignedSignature ///< \since 22.02
+        UnsignedSignature, ///< \since 22.02
+        G10cPgpSignatureDetached, ///< nonstandardized signature type \since 25.01
     };
 
     /**
@@ -994,6 +1013,23 @@ void POPPLER_QT6_EXPORT setNSSDir(const QString &pathURL);
   \since 21.01
 */
 void POPPLER_QT6_EXPORT setNSSPasswordCallback(const std::function<char *(const char *)> &f);
+
+/**
+ * Allow pgp signatures in pdf files (standard-extension, experimental)
+ * Not supported for NSS backend
+ *
+ * \param allowed new value for pgp signatures allowed
+ *
+ * \since 25.03
+ */
+void POPPLER_QT6_EXPORT setPgpSignaturesAllowed(bool allowed);
+
+/**
+ * \returns if pgp signatures are allowed.
+ *
+ * \since 25.03
+ */
+bool POPPLER_QT6_EXPORT isPgpSignaturesAllowed();
 }
 
 #endif

tsdgeos/poppler_mirror

qt5/src/poppler-form.cc
@@ -52,6 +52,9 @@
 #ifdef ENABLE_NSS3
 #    include <NSSCryptoSignBackend.h>
 #endif
+#ifdef ENABLE_GPGME
+#    include <GPGMECryptoSignBackend.h>
+#endif
 
 #include "poppler-page-private.h"
 #include "poppler-private.h"
@@ -617,6 +620,7 @@ public:
     bool is_self_signed;
     bool is_null;
     CertificateInfo::KeyLocation keyLocation;
+    CertificateInfo::CertificateType certificateType;
 };
 
 CertificateInfo::CertificateInfo() : d_ptr(new CertificateInfoPrivate())
@@ -655,6 +659,12 @@ QByteArray CertificateInfo::serialNumber() const
 {
     Q_D(const CertificateInfo);
     return d->serial_number;
+}
+
+CertificateInfo::CertificateType CertificateInfo::certificateType() const
+{
+    Q_D(const CertificateInfo);
+    return d->certificateType;
 }
 
 QString CertificateInfo::issuerInfo(EntityInfoKey key) const
@@ -975,6 +985,9 @@ FormFieldSignature::SignatureType FormFieldSignature::signatureType() const
     case CryptoSign::SignatureType::unsigned_signature_field:
         sigType = UnsignedSignature;
         break;
+    case CryptoSign::SignatureType::g10c_pgp_signature_detached:
+        sigType = G10cPgpSignatureDetached;
+        break;
     }
     return sigType;
 }
@@ -1001,6 +1014,17 @@ static CertificateInfo::KeyLocation fromPopplerCore(KeyLocation location)
     return CertificateInfo::KeyLocation::Unknown;
 }
 
+static CertificateInfo::CertificateType fromPopplerCore(CertificateType type)
+{
+    switch (type) {
+    case CertificateType::PGP:
+        return CertificateInfo::CertificateType::PGP;
+    case CertificateType::X509:
+        return CertificateInfo::CertificateType::X509;
+    }
+    return CertificateInfo::CertificateType::X509; // fallback
+}
+
 static CertificateInfoPrivate *createCertificateInfoPrivate(const X509CertificateInfo *ci)
 {
     CertificateInfoPrivate *certPriv = new CertificateInfoPrivate;
@@ -1009,6 +1033,7 @@ static CertificateInfoPrivate *createCertificateInfoPrivate(const X509Certificat
         certPriv->version = ci->getVersion();
         certPriv->ku_extensions = ci->getKeyUsageExtensions();
         certPriv->keyLocation = fromPopplerCore(ci->getKeyLocation());
+        certPriv->certificateType = fromPopplerCore(ci->getCertificateType());
 
         const GooString &certSerial = ci->getSerialNumber();
         certPriv->serial_number = QByteArray(certSerial.c_str(), certSerial.getLength());
@@ -1342,4 +1367,22 @@ void setNSSPasswordCallback(const std::function<char *(const char *)> &f)
 #endif
 }
 
+void setPgpSignaturesAllowed(bool allowed)
+{
+#ifdef ENABLE_GPGME
+    GpgSignatureConfiguration::setPgpSignaturesAllowed(allowed);
+#else
+    qWarning() << "Trying to enable pgp signatures, but pgp not enabled in this build";
+    (void)allowed;
+#endif
+}
+
+bool arePgpSignaturesAllowed()
+{
+#ifdef ENABLE_GPGME
+    return GpgSignatureConfiguration::arePgpSignaturesAllowed();
+#else
+    return false;
+#endif
+}
 }

qt5/src/poppler-form.h
@@ -534,6 +534,17 @@ public:
     Q_DECLARE_FLAGS(KeyUsageExtensions, KeyUsageExtension)
 
     /**
+     * The type of the certificate
+     *
+     * \since 25.03
+     */
+    enum class CertificateType
+    {
+        X509,
+        PGP
+    };
+
+    /**
       Predefined keys for elements in an entity's distinguished name.
      */
     enum EntityInfoKey
@@ -632,6 +643,13 @@ public:
       Returns true if certificate is self-signed otherwise returns false.
      */
     bool isSelfSigned() const;
+
+    /**
+     * \return certificate type
+     *
+     * \since 25.03
+     */
+    CertificateType certificateType() const;
 
     /**
       The DER encoded certificate.
@@ -839,7 +857,8 @@ public:
         AdbePkcs7detached,
         EtsiCAdESdetached,
         UnknownSignatureType, ///< \since 0.90
-        UnsignedSignature ///< \since 22.02
+        UnsignedSignature, ///< \since 22.02
+        G10cPgpSignatureDetached, ///< non standard signature type \since 25.01
     };
 
     /**
@@ -1035,6 +1054,23 @@ void POPPLER_QT5_EXPORT setNSSDir(const QString &pathURL);
   \since 21.01
 */
 void POPPLER_QT5_EXPORT setNSSPasswordCallback(const std::function<char *(const char *)> &f);
+
+/**
+ * Allow pgp signatures in pdf files (standard-extension, experimental)
+ * Not supported for NSS backend
+ *
+ * \param allowed new value for pgp signatures allowed
+ *
+ * \since 25.03
+ */
+void POPPLER_QT5_EXPORT setPgpSignaturesAllowed(bool allowed);
+
+/**
+ * \returns if pgp signatures are allowed.
+ *
+ * \since 25.03
+ */
+bool POPPLER_QT5_EXPORT arePgpSignaturesAllowed();
 }
 
 #endif

qt6/src/poppler-form.cc
@@ -52,6 +52,9 @@
 #ifdef ENABLE_NSS3
 #    include <NSSCryptoSignBackend.h>
 #endif
+#ifdef ENABLE_GPGME
+#    include <GPGMECryptoSignBackend.h>
+#endif
 
 #include "poppler-page-private.h"
 #include "poppler-private.h"
@@ -617,6 +620,7 @@ public:
     bool is_self_signed;
     bool is_null;
     bool is_qualified;
+    CertificateInfo::CertificateType certificateType;
     CertificateInfo::KeyLocation keyLocation;
 };
 
@@ -662,6 +666,12 @@ bool CertificateInfo::isQualified() const
 {
     Q_D(const CertificateInfo);
     return d->is_qualified;
+}
+
+CertificateInfo::CertificateType CertificateInfo::certificateType() const
+{
+    Q_D(const CertificateInfo);
+    return d->certificateType;
 }
 
 QString CertificateInfo::issuerInfo(EntityInfoKey key) const
@@ -979,6 +989,9 @@ FormFieldSignature::SignatureType FormFieldSignature::signatureType() const
     case CryptoSign::SignatureType::unknown_signature_type:
         sigType = UnknownSignatureType;
         break;
+    case CryptoSign::SignatureType::g10c_pgp_signature_detached:
+        sigType = G10cPgpSignatureDetached;
+        break;
     case CryptoSign::SignatureType::unsigned_signature_field:
         sigType = UnsignedSignature;
         break;
@@ -991,6 +1004,17 @@ SignatureValidationInfo FormFieldSignature::validate(ValidateOptions opt) const
     auto tempResult = validateAsync(opt);
     tempResult.first.d_ptr->certificate_status = validateResult();
     return tempResult.first;
+}
+
+static CertificateInfo::CertificateType fromPopplerCore(CertificateType type)
+{
+    switch (type) {
+    case CertificateType::PGP:
+        return CertificateInfo::CertificateType::PGP;
+    case CertificateType::X509:
+        return CertificateInfo::CertificateType::X509;
+    }
+    return CertificateInfo::CertificateType::X509; // fallback
 }
 
 static CertificateInfo::KeyLocation fromPopplerCore(KeyLocation location)
@@ -1016,6 +1040,7 @@ static CertificateInfoPrivate *createCertificateInfoPrivate(const X509Certificat
         certPriv->version = ci->getVersion();
         certPriv->ku_extensions = ci->getKeyUsageExtensions();
         certPriv->keyLocation = fromPopplerCore(ci->getKeyLocation());
+        certPriv->certificateType = fromPopplerCore(ci->getCertificateType());
 
         const GooString &certSerial = ci->getSerialNumber();
         certPriv->serial_number = QByteArray(certSerial.c_str(), certSerial.getLength());
@@ -1362,6 +1387,25 @@ void setNSSPasswordCallback(const std::function<char *(const char *)> &f)
 #else
     qWarning() << "setNSSPasswordCallback called but this poppler is built without NSS support";
     (void)f;
+#endif
+}
+
+void setPgpSignaturesAllowed(bool allowed)
+{
+#ifdef ENABLE_GPGME
+    GpgSignatureConfiguration::setPgpSignaturesAllowed(allowed);
+#else
+    qWarning() << "Trying to enable pgp signatures, but pgp not enabled in this build";
+    (void)allowed;
+#endif
+}
+
+bool arePgpSignaturesAllowed()
+{
+#ifdef ENABLE_GPGME
+    return GpgSignatureConfiguration::arePgpSignaturesAllowed();
+#else
+    return false;
 #endif
 }
 }

qt6/src/poppler-form.h
@@ -510,6 +510,17 @@ public:
         Organization,
     };
 
+    /**
+     * The type of the certificate
+     *
+     * \since 25.03
+     */
+    enum class CertificateType
+    {
+        X509,
+        PGP
+    };
+
     /** A signing key can be located in different places
      sometimes. For the user, it might be easier to pick
      the key located on a card if it has some visual
@@ -605,6 +616,13 @@ public:
      * https://en.wikipedia.org/wiki/Qualified_electronic_signature
      */
     bool isQualified() const;
+
+    /**
+     * \return certificate type
+     *
+     * \since 25.03
+     */
+    CertificateType certificateType() const;
 
     /**
       The DER encoded certificate.
@@ -798,7 +816,8 @@ public:
         AdbePkcs7sha1,
         AdbePkcs7detached,
         EtsiCAdESdetached,
-        UnsignedSignature ///< \since 22.02
+        UnsignedSignature, ///< \since 22.02
+        G10cPgpSignatureDetached, ///< nonstandardized signature type \since 25.01
     };
 
     /**
@@ -998,6 +1017,23 @@ void POPPLER_QT6_EXPORT setNSSDir(const QString &pathURL);
   \since 21.01
 */
 void POPPLER_QT6_EXPORT setNSSPasswordCallback(const std::function<char *(const char *)> &f);
+
+/**
+ * Allow pgp signatures in pdf files (standard-extension, experimental)
+ * Not supported for NSS backend
+ *
+ * \param allowed new value for pgp signatures allowed
+ *
+ * \since 25.03
+ */
+void POPPLER_QT6_EXPORT setPgpSignaturesAllowed(bool allowed);
+
+/**
+ * \returns if pgp signatures are allowed.
+ *
+ * \since 25.03
+ */
+bool POPPLER_QT6_EXPORT arePgpSignaturesAllowed();
 }
 
 #endif