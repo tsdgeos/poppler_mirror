5e54d95c6d569bfa048544fbc05be99399c66430
Albert Astals Cid <aacid@kde.org>
2025-04-26 13:09:05 +0200

    Update (C)

--- poppler/Dict.cc
+++ poppler/Dict.cc
@@ -23,6 +23,7 @@
 // Copyright (C) 2014 Scott West <scott.gregory.west@gmail.com>
 // Copyright (C) 2017 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2025 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
