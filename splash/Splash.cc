c9b3d99c097c2af049438000fd78210c8f3559ca
Albert Astals Cid <aacid@kde.org>
2025-08-09 21:51:45 +0200

    Splash::dumpXPath: take path by const &

--- splash/Splash.cc
+++ splash/Splash.cc
@@ -6406,13 +6406,13 @@ void Splash::dumpPath(const SplashPath &path)
     }
 }
 
-void Splash::dumpXPath(SplashXPath *path)
+void Splash::dumpXPath(const SplashXPath &path)
 {
     int i;
 
-    for (i = 0; i < path->length; ++i) {
-        printf("  %4d: x0=%8.2f y0=%8.2f x1=%8.2f y1=%8.2f %s%s%s\n", i, (double)path->segs[i].x0, (double)path->segs[i].y0, (double)path->segs[i].x1, (double)path->segs[i].y1, (path->segs[i].flags & splashXPathHoriz) ? "H" : " ",
-               (path->segs[i].flags & splashXPathVert) ? "V" : " ", (path->segs[i].flags & splashXPathFlip) ? "P" : " ");
+    for (i = 0; i < path.length; ++i) {
+        printf("  %4d: x0=%8.2f y0=%8.2f x1=%8.2f y1=%8.2f %s%s%s\n", i, (double)path.segs[i].x0, (double)path.segs[i].y0, (double)path.segs[i].x1, (double)path.segs[i].y1, (path.segs[i].flags & splashXPathHoriz) ? "H" : " ",
+               (path.segs[i].flags & splashXPathVert) ? "V" : " ", (path.segs[i].flags & splashXPathFlip) ? "P" : " ");
     }
 }
 
