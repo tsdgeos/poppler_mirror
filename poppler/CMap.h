5070962f19d40206861aba07c2af8a185b9e9ed9
Albert Astals Cid <aacid@kde.org>
2025-10-16 10:08:10 +0200

    Update (C)

--- poppler/CMap.h
+++ poppler/CMap.h
@@ -17,6 +17,7 @@
 // Copyright (C) 2009, 2018-2020, 2022, 2024, 2025 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2012, 2017 Adrian Johnson <ajohnson@redneon.com>
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
+// Copyright (C) 2025 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
