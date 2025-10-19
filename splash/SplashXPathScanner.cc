6fa694c2bb960f46fefdb83683608304205a6d67
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2025-10-19 02:09:04 +0200

    [SplashXPathScanner] Reduce variable scope

--- splash/SplashXPathScanner.cc
+++ splash/SplashXPathScanner.cc
@@ -316,17 +316,12 @@ inline void SplashXPathScanner::addIntersection(double segYMin, int y, int x0, i
 
 void SplashXPathScanner::renderAALine(SplashBitmap *aaBuf, int *x0, int *x1, int y, bool adjustVertLine) const
 {
-    int xx0, xx1, xx, xxMin, xxMax, yy, yyMax, interCount;
-    size_t interIdx;
-    unsigned char mask;
-    SplashColorPtr p;
-
     memset(aaBuf->getDataPtr(), 0, aaBuf->getRowSize() * aaBuf->getHeight());
-    xxMin = aaBuf->getWidth();
-    xxMax = -1;
+    int xxMin = aaBuf->getWidth();
+    int xxMax = -1;
     if (yMin <= yMax) {
-        yy = 0;
-        yyMax = splashAASize - 1;
+        int yy = 0;
+        int yyMax = splashAASize - 1;
         // clamp start and end position
         if (yMin > splashAASize * y) {
             yy = yMin - splashAASize * y;
@@ -338,11 +333,11 @@ void SplashXPathScanner::renderAALine(SplashBitmap *aaBuf, int *x0, int *x1, int
         int eoMask = eo ? 0x1 : ~0;
         for (; yy <= yyMax; ++yy) {
             const auto &line = allIntersections[splashAASize * y + yy - yMin];
-            interIdx = 0;
-            interCount = 0;
+            size_t interIdx = 0;
+            int interCount = 0;
             while (interIdx < line.size()) {
-                xx0 = line[interIdx].x0;
-                xx1 = line[interIdx].x1;
+                int xx0 = line[interIdx].x0;
+                int xx1 = line[interIdx].x1;
                 interCount += line[interIdx].count;
                 ++interIdx;
                 while (interIdx < line.size() && (line[interIdx].x0 <= xx1 || (interCount & eoMask))) {
@@ -361,10 +356,10 @@ void SplashXPathScanner::renderAALine(SplashBitmap *aaBuf, int *x0, int *x1, int
                 }
                 // set [xx0, xx1) to 1
                 if (xx0 < xx1) {
-                    xx = xx0;
-                    p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
+                    int xx = xx0;
+                    SplashColorPtr p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
                     if (xx & 7) {
-                        mask = adjustVertLine ? 0xff : 0xff >> (xx & 7);
+                        unsigned char mask = adjustVertLine ? 0xff : 0xff >> (xx & 7);
                         if (!adjustVertLine && (xx & ~7) == (xx1 & ~7)) {
                             mask &= (unsigned char)(0xff00 >> (xx1 & 7));
                         }
@@ -396,13 +391,8 @@ void SplashXPathScanner::renderAALine(SplashBitmap *aaBuf, int *x0, int *x1, int
 
 void SplashXPathScanner::clipAALine(SplashBitmap *aaBuf, const int *x0, const int *x1, int y) const
 {
-    int xx0, xx1, xx, yy, yyMin, yyMax, interCount;
-    size_t interIdx;
-    unsigned char mask;
-    SplashColorPtr p;
-
-    yyMin = 0;
-    yyMax = splashAASize - 1;
+    int yyMin = 0;
+    int yyMax = splashAASize - 1;
     // clamp start and end position
     if (yMin > splashAASize * y) {
         yyMin = yMin - splashAASize * y;
@@ -411,19 +401,19 @@ void SplashXPathScanner::clipAALine(SplashBitmap *aaBuf, const int *x0, const in
         yyMax = yMax - splashAASize * y;
     }
     int eoMask = eo ? 0x1 : ~0;
-    for (yy = 0; yy < splashAASize; ++yy) {
-        xx = *x0 * splashAASize;
+    for (int yy = 0; yy < splashAASize; ++yy) {
+        int xx = *x0 * splashAASize;
         if (yy >= yyMin && yy <= yyMax) {
             const int intersectionIndex = splashAASize * y + yy - yMin;
             if (unlikely(intersectionIndex < 0 || (unsigned)intersectionIndex >= allIntersections.size())) {
                 break;
             }
             const auto &line = allIntersections[intersectionIndex];
-            interIdx = 0;
-            interCount = 0;
+            size_t interIdx = 0;
+            int interCount = 0;
             while (interIdx < line.size() && xx < (*x1 + 1) * splashAASize) {
-                xx0 = line[interIdx].x0;
-                xx1 = line[interIdx].x1;
+                int xx0 = line[interIdx].x0;
+                int xx1 = line[interIdx].x1;
                 interCount += line[interIdx].count;
                 ++interIdx;
                 while (interIdx < line.size() && (line[interIdx].x0 <= xx1 || (interCount & eoMask))) {
@@ -438,9 +428,9 @@ void SplashXPathScanner::clipAALine(SplashBitmap *aaBuf, const int *x0, const in
                 }
                 // set [xx, xx0) to 0
                 if (xx < xx0) {
-                    p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
+                    SplashColorPtr p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
                     if (xx & 7) {
-                        mask = (unsigned char)(0xff00 >> (xx & 7));
+                        unsigned char mask = (unsigned char)(0xff00 >> (xx & 7));
                         if ((xx & ~7) == (xx0 & ~7)) {
                             mask |= 0xff >> (xx0 & 7);
                         }
@@ -459,15 +449,15 @@ void SplashXPathScanner::clipAALine(SplashBitmap *aaBuf, const int *x0, const in
                 }
             }
         }
-        xx0 = (*x1 + 1) * splashAASize;
+        int xx0 = (*x1 + 1) * splashAASize;
         if (xx0 > aaBuf->getWidth()) {
             xx0 = aaBuf->getWidth();
         }
         // set [xx, xx0) to 0
         if (xx < xx0 && xx >= 0) {
-            p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
+            SplashColorPtr p = aaBuf->getDataPtr() + yy * aaBuf->getRowSize() + (xx >> 3);
             if (xx & 7) {
-                mask = (unsigned char)(0xff00 >> (xx & 7));
+                unsigned char mask = (unsigned char)(0xff00 >> (xx & 7));
                 if ((xx & ~7) == (xx0 & ~7)) {
                     mask &= 0xff >> (xx0 & 7);
                 }
