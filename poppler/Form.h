c8147d255e604cc42f44f45159a58db86df3825f
Albert Astals Cid <aacid@kde.org>
2025-11-30 16:18:05 +0100

    Update (C)

--- poppler/Form.h
+++ poppler/Form.h
@@ -6,7 +6,7 @@
 //
 // Copyright 2006 Julien Rebetez <julienr@svn.gnome.org>
 // Copyright 2007, 2008, 2011 Carlos Garcia Campos <carlosgc@gnome.org>
-// Copyright 2007-2010, 2012, 2015-2024 Albert Astals Cid <aacid@kde.org>
+// Copyright 2007-2010, 2012, 2015-2025 Albert Astals Cid <aacid@kde.org>
 // Copyright 2010 Mark Riedesel <mark@klowner.com>
 // Copyright 2011 Pino Toscano <pino@kde.org>
 // Copyright 2012 Fabio D'Urso <fabiodurso@hotmail.it>
