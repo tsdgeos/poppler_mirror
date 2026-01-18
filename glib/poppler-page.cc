tsdgeos/poppler_mirror

commit 0a4419c61d5f625347e06e901fc52027ac3b4e72

TextPage: stop manual refcount
Use smartpointers to manage lifetime instead

glib/poppler-page.cc
@@ -83,9 +83,6 @@ static void poppler_page_finalize(GObject *object)
     g_object_unref(page->document);
     page->document = nullptr;
 
-    if (page->text != nullptr) {
-        page->text->decRefCnt();
-    }
     /* page->page is owned by the document */
 
     G_OBJECT_CLASS(poppler_page_parent_class)->finalize(object);
@@ -259,21 +256,18 @@ PopplerPageTransition *poppler_page_get_transition(PopplerPage *page)
 static TextPage *poppler_page_get_text_page(PopplerPage *page)
 {
     if (page->text == nullptr) {
-        TextOutputDev *text_dev;
 
-        text_dev = new TextOutputDev(nullptr, true, 0, false, false);
-        std::unique_ptr<Gfx> gfx = page->page->createGfx(text_dev, 72.0, 72.0, 0, false, /* useMediaBox */
+        auto text_dev = std::make_unique<TextOutputDev>(nullptr, true, 0, false, false);
+        std::unique_ptr<Gfx> gfx = page->page->createGfx(text_dev.get(), 72.0, 72.0, 0, false, /* useMediaBox */
                                                          true, /* Crop */
                                                          -1, -1, -1, -1, nullptr, nullptr);
         page->page->display(gfx.get());
         text_dev->endPage();
 
         page->text = text_dev->takeText();
-        gfx.reset(); // deletion order here is important, gfx before text_dev
-        delete text_dev;
     }
 
-    return page->text;
+    return page->text.get();
 }
 
 static bool annots_display_decide_cb(Annot *annot, void *user_data)
@@ -317,7 +311,7 @@ void poppler_page_render_full(PopplerPage *page, cairo_t *cairo, gboolean printi
     output_dev->setPrinting(printing);
 
     if (!printing && page->text == nullptr) {
-        page->text = new TextPage(false);
+        page->text = std::make_shared<TextPage>(false);
         output_dev->setTextPage(page->text);
     }
 

qt5/src/poppler-page.cc
@@ -387,7 +387,7 @@ Link *PageData::convertLinkActionToLink(::LinkAction *a, DocumentData *parentDoc
     return popplerLink;
 }
 
-inline TextPage *PageData::prepareTextSearch(const QString &text, Page::Rotation rotate, QVector<Unicode> *u)
+inline std::unique_ptr<TextPage> PageData::prepareTextSearch(const QString &text, Page::Rotation rotate, QVector<Unicode> *u)
 {
     *u = text.toUcs4();
 
@@ -396,7 +396,7 @@ inline TextPage *PageData::prepareTextSearch(const QString &text, Page::Rotation
     // fetch ourselves a textpage
     TextOutputDev td(nullptr, true, 0, false, false);
     parentDoc->doc->displayPage(&td, index + 1, 72, 72, rotation, false, true, false, nullptr, nullptr, nullptr, nullptr, true);
-    TextPage *textPage = td.takeText();
+    std::unique_ptr<TextPage> textPage = td.takeText();
 
     return textPage;
 }
@@ -718,11 +718,9 @@ bool Page::search(const QString &text, double &sLeft, double &sTop, double &sRig
     const bool sCase = caseSensitive == Page::CaseSensitive ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    const bool found = m_page->performSingleTextSearch(textPage, u, sLeft, sTop, sRight, sBottom, direction, sCase, false, false, false);
-
-    textPage->decRefCnt();
+    const bool found = m_page->performSingleTextSearch(textPage.get(), u, sLeft, sTop, sRight, sBottom, direction, sCase, false, false, false);
 
     return found;
 }
@@ -735,11 +733,9 @@ bool Page::search(const QString &text, double &sLeft, double &sTop, double &sRig
     const bool sAcrossLines = flags.testFlag(AcrossLines) ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    const bool found = m_page->performSingleTextSearch(textPage, u, sLeft, sTop, sRight, sBottom, direction, sCase, sWords, sDiacritics, sAcrossLines);
-
-    textPage->decRefCnt();
+    const bool found = m_page->performSingleTextSearch(textPage.get(), u, sLeft, sTop, sRight, sBottom, direction, sCase, sWords, sDiacritics, sAcrossLines);
 
     return found;
 }
@@ -749,11 +745,9 @@ QList<QRectF> Page::search(const QString &text, SearchMode caseSensitive, Rotati
     const bool sCase = caseSensitive == Page::CaseSensitive ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    const QList<QRectF> results = m_page->performMultipleTextSearch(textPage, u, sCase, false, false, false);
-
-    textPage->decRefCnt();
+    const QList<QRectF> results = m_page->performMultipleTextSearch(textPage.get(), u, sCase, false, false, false);
 
     return results;
 }
@@ -766,11 +760,9 @@ QList<QRectF> Page::search(const QString &text, SearchFlags flags, Rotation rota
     const bool sAcrossLines = flags.testFlag(AcrossLines) ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    const QList<QRectF> results = m_page->performMultipleTextSearch(textPage, u, sCase, sWords, sDiacritics, sAcrossLines);
-
-    textPage->decRefCnt();
+    const QList<QRectF> results = m_page->performMultipleTextSearch(textPage.get(), u, sCase, sWords, sDiacritics, sAcrossLines);
 
     return results;
 }

qt6/src/poppler-page.cc
@@ -387,7 +387,7 @@ std::unique_ptr<Link> PageData::convertLinkActionToLink(::LinkAction *a, Documen
     return popplerLink;
 }
 
-inline TextPage *PageData::prepareTextSearch(const QString &text, Page::Rotation rotate, QVector<Unicode> *u)
+inline std::unique_ptr<TextPage> PageData::prepareTextSearch(const QString &text, Page::Rotation rotate, QVector<Unicode> *u)
 {
     *u = text.toUcs4();
 
@@ -396,7 +396,7 @@ inline TextPage *PageData::prepareTextSearch(const QString &text, Page::Rotation
     // fetch ourselves a textpage
     TextOutputDev td(nullptr, true, 0, false, false);
     parentDoc->doc->displayPage(&td, index + 1, 72, 72, rotation, false, true, false, nullptr, nullptr, nullptr, nullptr, true);
-    TextPage *textPage = td.takeText();
+    std::unique_ptr<TextPage> textPage = td.takeText();
 
     return textPage;
 }
@@ -721,11 +721,9 @@ bool Page::search(const QString &text, double &sLeft, double &sTop, double &sRig
     const bool sAcrossLines = flags.testFlag(AcrossLines) ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    const bool found = m_page->performSingleTextSearch(textPage, u, sLeft, sTop, sRight, sBottom, direction, sCase, sWords, sDiacritics, sAcrossLines);
-
-    textPage->decRefCnt();
+    const bool found = m_page->performSingleTextSearch(textPage.get(), u, sLeft, sTop, sRight, sBottom, direction, sCase, sWords, sDiacritics, sAcrossLines);
 
     return found;
 }
@@ -738,11 +736,9 @@ QList<QRectF> Page::search(const QString &text, SearchFlags flags, Rotation rota
     const bool sAcrossLines = flags.testFlag(AcrossLines) ? true : false;
 
     QVector<Unicode> u;
-    TextPage *textPage = m_page->prepareTextSearch(text, rotate, &u);
+    std::unique_ptr<TextPage> textPage = m_page->prepareTextSearch(text, rotate, &u);
 
-    QList<QRectF> results = m_page->performMultipleTextSearch(textPage, u, sCase, sWords, sDiacritics, sAcrossLines);
-
-    textPage->decRefCnt();
+    QList<QRectF> results = m_page->performMultipleTextSearch(textPage.get(), u, sCase, sWords, sDiacritics, sAcrossLines);
 
     return results;
 }

cpp/poppler-page.cpp
@@ -231,7 +231,7 @@ bool page::search(const ustring &text, rectf &r, search_direction_enum direction
 
     TextOutputDev td(nullptr, true, 0, false, false);
     d->doc->doc->displayPage(&td, d->index + 1, 72, 72, rotation_value, false, true, false);
-    TextPage *text_page = td.takeText();
+    std::unique_ptr<TextPage> text_page = td.takeText();
 
     switch (direction) {
     case search_from_top:
@@ -245,7 +245,6 @@ bool page::search(const ustring &text, rectf &r, search_direction_enum direction
         break;
     }
 
-    text_page->decRefCnt();
     r.set_left(rect_left);
     r.set_top(rect_top);
     r.set_right(rect_right);

glib/poppler-private.h
@@ -66,7 +66,7 @@ struct _PopplerPage
     PopplerDocument *document;
     Page *page;
     int index;
-    TextPage *text;
+    std::shared_ptr<TextPage> text;
 };
 
 struct _PopplerFormField

poppler/CairoOutputDev.cc
@@ -213,10 +213,6 @@ CairoOutputDev::~CairoOutputDev()
     if (shape) {
         cairo_pattern_destroy(shape);
     }
-    if (textPage) {
-        textPage->decRefCnt();
-    }
-    delete actualText;
 }
 
 void CairoOutputDev::setCairo(cairo_t *c)
@@ -248,19 +244,12 @@ bool CairoOutputDev::isPDF()
     return false;
 }
 
-void CairoOutputDev::setTextPage(TextPage *text)
+void CairoOutputDev::setTextPage(std::shared_ptr<TextPage> text)
 {
+    actualText.reset();
+    textPage = std::move(text);
     if (textPage) {
-        textPage->decRefCnt();
-    }
-    delete actualText;
-    if (text) {
-        textPage = text;
-        textPage->incRefCnt();
-        actualText = new ActualText(text);
-    } else {
-        textPage = nullptr;
-        actualText = nullptr;
+        actualText = std::make_unique<ActualText>(textPage.get());
     }
 }
 

poppler/CairoOutputDev.h
@@ -248,7 +248,7 @@ public:
     bool isReverseVideo() { return false; }
 
     void setCairo(cairo_t *cr);
-    void setTextPage(TextPage *text);
+    void setTextPage(std::shared_ptr<TextPage> text);
     void setPrinting(bool printingA)
     {
         printing = printingA;
@@ -360,8 +360,8 @@ protected:
     std::set<std::string> emittedDestinations;
     std::map<int, int> pdfPageToCairoPageMap;
 
-    TextPage *textPage; // text for the current page
-    ActualText *actualText;
+    std::shared_ptr<TextPage> textPage; // text for the current page
+    std::unique_ptr<ActualText> actualText;
 
     cairo_pattern_t *group;
     cairo_pattern_t *shape;

poppler/TextOutputDev.cc
@@ -2442,7 +2442,6 @@ TextPage::TextPage(bool rawOrderA, bool discardDiagA)
 {
     int rot;
 
-    refCnt = 1;
     rawOrder = rawOrderA;
     discardDiag = discardDiagA;
     curWord = nullptr;
@@ -2470,18 +2469,6 @@ TextPage::TextPage(bool rawOrderA, bool discardDiagA)
 TextPage::~TextPage()
 {
     clear();
-}
-
-void TextPage::incRefCnt()
-{
-    refCnt++;
-}
-
-void TextPage::decRefCnt()
-{
-    if (--refCnt == 0) {
-        delete this;
-    }
 }
 
 void TextPage::startPage(const GfxState *state)
@@ -5494,16 +5481,12 @@ std::unique_ptr<TextWordList> TextPage::makeWordList(bool physLayout)
 //------------------------------------------------------------------------
 ActualText::ActualText(TextPage *out)
 {
-    out->incRefCnt();
     text = out;
     actualText = nullptr;
     actualTextNBytes = 0;
 }
 
-ActualText::~ActualText()
-{
-    text->decRefCnt();
-}
+ActualText::~ActualText() = default;
 
 void ActualText::addChar(const GfxState *state, double x, double y, double dx, double dy, CharCode c, int nBytes, const Unicode *u, int uLen)
 {
@@ -5590,8 +5573,8 @@ TextOutputDev::TextOutputDev(const char *fileName, bool physLayoutA, double fixe
     }
 
     // set up text object
-    text = new TextPage(rawOrderA, discardDiagA);
-    actualText = new ActualText(text);
+    text = std::make_unique<TextPage>(rawOrderA, discardDiagA);
+    actualText = std::make_unique<ActualText>(text.get());
 }
 
 TextOutputDev::TextOutputDev(TextOutputFunc func, void *stream, bool physLayoutA, double fixedPitchA, bool rawOrderA, bool discardDiagA)
@@ -5604,8 +5587,8 @@ TextOutputDev::TextOutputDev(TextOutputFunc func, void *stream, bool physLayoutA
     rawOrder = rawOrderA;
     discardDiag = discardDiagA;
     doHTML = false;
-    text = new TextPage(rawOrderA, discardDiagA);
-    actualText = new ActualText(text);
+    text = std::make_unique<TextPage>(rawOrderA, discardDiagA);
+    actualText = std::make_unique<ActualText>(text.get());
     textEOL = EndOfLineKind::eolUnix;
     textPageBreaks = true;
     ok = true;
@@ -5617,10 +5600,6 @@ TextOutputDev::~TextOutputDev()
     if (needClose) {
         fclose((FILE *)outputStream);
     }
-    if (text) {
-        text->decRefCnt();
-    }
-    delete actualText;
 }
 
 void TextOutputDev::startPage(int /*pageNum*/, GfxState *state, XRef * /*xref*/)
@@ -5856,14 +5835,12 @@ std::unique_ptr<TextWordList> TextOutputDev::makeWordList()
     return text->makeWordList(physLayout);
 }
 
-TextPage *TextOutputDev::takeText()
+std::unique_ptr<TextPage> TextOutputDev::takeText()
 {
-    TextPage *ret;
+    auto ret = std::move(text);
 
-    ret = text;
-    text = new TextPage(rawOrder, discardDiag);
-    delete actualText;
-    actualText = new ActualText(text);
+    text = std::make_unique<TextPage>(rawOrder, discardDiag);
+    actualText = std::make_unique<ActualText>(text.get());
     return ret;
 }
 

commit 64ea4dce5324ce3be10e82a2ba2a4037ab353290

glib: Fix memory leak introduced in 0a4419c61d5f625347e06e901fc52027ac3b4e72
glib objects are not C++ and thus we need to manually
reset the smart pointers :(

glib/poppler-page.cc
@@ -82,6 +82,7 @@ static void poppler_page_finalize(GObject *object)
 
     g_object_unref(page->document);
     page->document = nullptr;
+    page->text.reset();
 
     /* page->page is owned by the document */
 