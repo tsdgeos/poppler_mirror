fb66873f5c4d9e459a15b898e7f9ea406a8b0f06
Albert Astals Cid <aacid@kde.org>
2026-03-22 16:47:22 +0100

    Remove unused expertEncoding

--- poppler/FontEncodingTables.h
+++ poppler/FontEncodingTables.h
@@ -13,7 +13,6 @@ extern const char *macRomanEncoding[];
 extern const char *macExpertEncoding[];
 extern const char *winAnsiEncoding[];
 extern const char *standardEncoding[];
-extern const char *expertEncoding[];
 extern const char *symbolEncoding[];
 extern const char *zapfDingbatsEncoding[];
 
