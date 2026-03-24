tsdgeos/poppler_mirror

commit 2e3e7f80256023a7b7fafc921e889bef5e4fc0bd

Convert char *const foo[] to std:array

fofi/FoFiType1C.cc
@@ -98,13 +98,11 @@ FoFiType1C::FoFiType1C(std::span<const unsigned char> data, PrivateTag /*unused*
 
 FoFiType1C::~FoFiType1C()
 {
-    int i;
-
-    if (encoding && encoding != fofiType1StandardEncoding && encoding != fofiType1ExpertEncoding) {
-        for (i = 0; i < 256; ++i) {
-            gfree(encoding[i]);
+    if (encoding && encoding != &fofiType1StandardEncoding && encoding != &fofiType1ExpertEncoding) {
+        for (const char *glyphName : *encoding) {
+            gfree(const_cast<char *>(glyphName));
         }
-        gfree(static_cast<void *>(encoding));
+        delete encoding;
     }
     if (privateDicts) {
         gfree(privateDicts);
@@ -122,7 +120,7 @@ const char *FoFiType1C::getName() const
     return name ? name->c_str() : nullptr;
 }
 
-char **FoFiType1C::getEncoding() const
+const std::array<const char *, 256> *FoFiType1C::getEncodingA() const
 {
     return encoding;
 }
@@ -251,7 +249,7 @@ void FoFiType1C::convertToType1(const char *psName, const char **newEncoding, bo
 
     // write the encoding
     (*outputFunc)(outputStream, "/Encoding ", 10);
-    if (!newEncoding && encoding == fofiType1StandardEncoding) {
+    if (!newEncoding && encoding == &fofiType1StandardEncoding) {
         (*outputFunc)(outputStream, "StandardEncoding def\n", 21);
     } else {
         (*outputFunc)(outputStream, "256 array\n", 10);
@@ -2257,16 +2255,14 @@ void FoFiType1C::buildEncoding()
     int pos, c, sid, nLeft, nSups, i, j;
 
     if (topDict.encodingOffset == 0) {
-        encoding = (char **)fofiType1StandardEncoding;
+        encoding = &fofiType1StandardEncoding;
 
     } else if (topDict.encodingOffset == 1) {
-        encoding = (char **)fofiType1ExpertEncoding;
+        encoding = &fofiType1ExpertEncoding;
 
     } else {
-        encoding = (char **)gmallocn(256, sizeof(char *));
-        for (i = 0; i < 256; ++i) {
-            encoding[i] = nullptr;
-        }
+        auto *customEncoding = new std::array<const char *, 256>();
+        customEncoding->fill(nullptr);
         pos = topDict.encodingOffset;
         encFormat = getU8(pos++, &parsedOk);
         if (!parsedOk) {
@@ -2285,10 +2281,8 @@ void FoFiType1C::buildEncoding()
                 if (!parsedOk) {
                     return;
                 }
-                if (encoding[c]) {
-                    gfree(encoding[c]);
-                }
-                encoding[c] = copyString(getString(charset[i], buf, &parsedOk));
+                gfree(const_cast<char *>((*customEncoding)[c]));
+                (*customEncoding)[c] = copyString(getString(charset[i], buf, &parsedOk));
             }
         } else if ((encFormat & 0x7f) == 1) {
             nRanges = getU8(pos++, &parsedOk);
@@ -2304,10 +2298,8 @@ void FoFiType1C::buildEncoding()
                 }
                 for (j = 0; j <= nLeft && nCodes < nGlyphs && nCodes < charsetLength; ++j) {
                     if (c < 256) {
-                        if (encoding[c]) {
-                            gfree(encoding[c]);
-                        }
-                        encoding[c] = copyString(getString(charset[nCodes], buf, &parsedOk));
+                        gfree(const_cast<char *>((*customEncoding)[c]));
+                        (*customEncoding)[c] = copyString(getString(charset[nCodes], buf, &parsedOk));
                     }
                     ++nCodes;
                     ++c;
@@ -2331,12 +2323,11 @@ void FoFiType1C::buildEncoding()
                 if (!parsedOk) {
                     return;
                 }
-                if (encoding[c]) {
-                    gfree(encoding[c]);
-                }
-                encoding[c] = copyString(getString(sid, buf, &parsedOk));
+                gfree(const_cast<char *>((*customEncoding)[c]));
+                (*customEncoding)[c] = copyString(getString(sid, buf, &parsedOk));
             }
         }
+        encoding = customEncoding;
     }
 }
 

fofi/FoFiEncodings.h
@@ -13,7 +13,7 @@
 // All changes made under the Poppler project to this file are licensed
 // under GPL version 2 or later
 //
-// Copyright (C) 2016, 2025 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2016, 2025, 2026 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2019 Volker Krause <vkrause@kde.org>
 //
 // To see a description of the changes please see the Changelog file that
@@ -24,12 +24,14 @@
 #ifndef FOFIENCODINGS_H
 #define FOFIENCODINGS_H
 
+#include <array>
+
 //------------------------------------------------------------------------
 // Type 1 and 1C font data
 //------------------------------------------------------------------------
 
-extern const char *const fofiType1StandardEncoding[256];
-extern const char *const fofiType1ExpertEncoding[256];
+extern const std::array<const char *, 256> fofiType1StandardEncoding;
+extern const std::array<const char *, 256> fofiType1ExpertEncoding;
 
 //------------------------------------------------------------------------
 // Type 1C font data

fofi/FoFiType1.cc
@@ -13,7 +13,7 @@
 // All changes made under the Poppler project to this file are licensed
 // under GPL version 2 or later
 //
-// Copyright (C) 2005, 2008, 2010, 2018, 2021-2025 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2005, 2008, 2010, 2018, 2021-2026 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2005 Kristian Høgsberg <krh@redhat.com>
 // Copyright (C) 2010 Jakub Wilk <jwilk@jwilk.net>
 // Copyright (C) 2014 Carlos Garcia Campos <carlosgc@gnome.org>
@@ -55,11 +55,11 @@ FoFiType1::FoFiType1(std::vector<unsigned char> &&fileA, PrivateTag /*unused*/) 
 
 FoFiType1::~FoFiType1()
 {
-    if (encoding && encoding != fofiType1StandardEncoding) {
-        for (int i = 0; i < 256; ++i) {
-            gfree(encoding[i]);
+    if (encoding && encoding != &fofiType1StandardEncoding) {
+        for (const char *glyphName : *encoding) {
+            gfree(const_cast<char *>(glyphName));
         }
-        gfree(static_cast<void *>(encoding));
+        delete encoding;
     }
 }
 
@@ -71,7 +71,7 @@ std::string FoFiType1::getName()
     return name;
 }
 
-char **FoFiType1::getEncoding()
+const std::array<const char *, 256> *FoFiType1::getEncodingA()
 {
     if (!parsed) {
         parse();
@@ -246,12 +246,10 @@ void FoFiType1::parse()
             }
 
             if (token2 == "StandardEncoding" && token3 == "def") {
-                encoding = (char **)fofiType1StandardEncoding;
+                encoding = &fofiType1StandardEncoding;
             } else if (token2 == "256" && token3 == "array") {
-                encoding = (char **)gmallocn(256, sizeof(char *));
-                for (int j = 0; j < 256; ++j) {
-                    encoding[j] = nullptr;
-                }
+                auto *customEncoding = new std::array<const char *, 256>();
+                customEncoding->fill(nullptr);
 
                 while (true) {
                     const std::optional<std::string_view> encodingToken = tokenizer.getToken();
@@ -288,14 +286,16 @@ void FoFiType1::parse()
                         }
 
                         if (code >= 0 && code < 256 && nameToken->length() > 1) {
-                            gfree(encoding[code]);
-                            encoding[code] = copyString(nameToken->data() + 1, nameToken->length() - 1);
+                            gfree(const_cast<char *>((*customEncoding)[code]));
+                            (*customEncoding)[code] = copyString(nameToken->data() + 1, nameToken->length() - 1);
                         }
 
                     } else if (encodingToken == "def") {
                         break;
                     }
                 }
+
+                encoding = customEncoding;
             }
         }
     }

fofi/FoFiType1.h
@@ -13,7 +13,7 @@
 // All changes made under the Poppler project to this file are licensed
 // under GPL version 2 or later
 //
-// Copyright (C) 2018, 2022, 2023 Albert Astals Cid <aacid@kde.org>
+// Copyright (C) 2018, 2022, 2023, 2026 Albert Astals Cid <aacid@kde.org>
 // Copyright (C) 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2025 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 //
@@ -51,7 +51,7 @@ public:
 
     // Return the encoding, as an array of 256 names (any of which may
     // be NULL).
-    char **getEncoding();
+    const std::array<const char *, 256> *getEncodingA();
 
     // Write a version of the Type 1 font file with a new encoding.
     void writeEncoded(const char **newEncoding, FoFiOutputFunc outputFunc, void *outputStream) const;
@@ -64,7 +64,7 @@ private:
     void undoPFB();
 
     std::string name;
-    char **encoding;
+    const std::array<const char *, 256> *encoding;
     bool parsed;
 };
 

fofi/FoFiType1C.h
@@ -172,7 +172,7 @@ public:
 
     // Return the encoding, as an array of 256 names (any of which may
     // be NULL).  This is only useful with 8-bit fonts.
-    char **getEncoding() const;
+    const std::array<const char *, 256> *getEncodingA() const;
 
     // Return the mapping from CIDs to GIDs, and return the number of
     // CIDs in *<nCIDs>.  This is only useful for CID fonts.
@@ -233,7 +233,7 @@ private:
     char *getString(int sid, char *buf, bool *ok) const;
 
     std::unique_ptr<GooString> name;
-    char **encoding;
+    const std::array<const char *, 256> *encoding;
 
     Type1CIndex nameIdx;
     Type1CIndex topDictIdx;

poppler/BuiltinFont.h
@@ -37,7 +37,7 @@ using GetWidthFunction = const BuiltinFontWidth *(*)(const char *str, size_t len
 struct BuiltinFont
 {
     const char *name;
-    const char *const *defaultBaseEnc;
+    const std::array<const char *, 256> *defaultBaseEnc;
     short ascent;
     short descent;
     short bbox[4];
@@ -75,20 +75,20 @@ const struct BuiltinFontWidth *TimesRomanWidthsLookup(const char *str, size_t le
 const struct BuiltinFontWidth *ZapfDingbatsWidthsLookup(const char *str, size_t len);
 }
 
-static const BuiltinFont builtinFonts[] = { { .name = "Courier", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -23, -250, 715, 805 }, .f = &CourierWidthsLookup },
-                                            { .name = "Courier-Bold", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -113, -250, 749, 801 }, .f = &CourierBoldWidthsLookup },
-                                            { .name = "Courier-BoldOblique", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -57, -250, 869, 801 }, .f = &CourierBoldObliqueWidthsLookup },
-                                            { .name = "Courier-Oblique", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -27, -250, 849, 805 }, .f = &CourierObliqueWidthsLookup },
-                                            { .name = "Helvetica", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -166, -225, 1000, 931 }, .f = &HelveticaWidthsLookup },
-                                            { .name = "Helvetica-Bold", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -170, -228, 1003, 962 }, .f = &HelveticaBoldWidthsLookup },
-                                            { .name = "Helvetica-BoldOblique", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -174, -228, 1114, 962 }, .f = &HelveticaBoldObliqueWidthsLookup },
-                                            { .name = "Helvetica-Oblique", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -170, -225, 1116, 931 }, .f = &HelveticaObliqueWidthsLookup },
-                                            { .name = "Symbol", .defaultBaseEnc = symbolEncoding, .ascent = 1010, .descent = -293, .bbox = { -180, -293, 1090, 1010 }, .f = &SymbolWidthsLookup },
-                                            { .name = "Times-Bold", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -168, -218, 1000, 935 }, .f = &TimesBoldWidthsLookup },
-                                            { .name = "Times-BoldItalic", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -200, -218, 996, 921 }, .f = &TimesBoldItalicWidthsLookup },
-                                            { .name = "Times-Italic", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -169, -217, 1010, 883 }, .f = &TimesItalicWidthsLookup },
-                                            { .name = "Times-Roman", .defaultBaseEnc = fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -168, -218, 1000, 898 }, .f = &TimesRomanWidthsLookup },
-                                            { .name = "ZapfDingbats", .defaultBaseEnc = zapfDingbatsEncoding, .ascent = 820, .descent = -143, .bbox = { -1, -143, 981, 820 }, .f = &ZapfDingbatsWidthsLookup } };
+static const BuiltinFont builtinFonts[] = { { .name = "Courier", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -23, -250, 715, 805 }, .f = &CourierWidthsLookup },
+                                            { .name = "Courier-Bold", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -113, -250, 749, 801 }, .f = &CourierBoldWidthsLookup },
+                                            { .name = "Courier-BoldOblique", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -57, -250, 869, 801 }, .f = &CourierBoldObliqueWidthsLookup },
+                                            { .name = "Courier-Oblique", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 629, .descent = -157, .bbox = { -27, -250, 849, 805 }, .f = &CourierObliqueWidthsLookup },
+                                            { .name = "Helvetica", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -166, -225, 1000, 931 }, .f = &HelveticaWidthsLookup },
+                                            { .name = "Helvetica-Bold", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -170, -228, 1003, 962 }, .f = &HelveticaBoldWidthsLookup },
+                                            { .name = "Helvetica-BoldOblique", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -174, -228, 1114, 962 }, .f = &HelveticaBoldObliqueWidthsLookup },
+                                            { .name = "Helvetica-Oblique", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 718, .descent = -207, .bbox = { -170, -225, 1116, 931 }, .f = &HelveticaObliqueWidthsLookup },
+                                            { .name = "Symbol", .defaultBaseEnc = &symbolEncoding, .ascent = 1010, .descent = -293, .bbox = { -180, -293, 1090, 1010 }, .f = &SymbolWidthsLookup },
+                                            { .name = "Times-Bold", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -168, -218, 1000, 935 }, .f = &TimesBoldWidthsLookup },
+                                            { .name = "Times-BoldItalic", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -200, -218, 996, 921 }, .f = &TimesBoldItalicWidthsLookup },
+                                            { .name = "Times-Italic", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -169, -217, 1010, 883 }, .f = &TimesItalicWidthsLookup },
+                                            { .name = "Times-Roman", .defaultBaseEnc = &fofiType1StandardEncoding, .ascent = 683, .descent = -217, .bbox = { -168, -218, 1000, 898 }, .f = &TimesRomanWidthsLookup },
+                                            { .name = "ZapfDingbats", .defaultBaseEnc = &zapfDingbatsEncoding, .ascent = 820, .descent = -143, .bbox = { -1, -143, 981, 820 }, .f = &ZapfDingbatsWidthsLookup } };
 
 static const BuiltinFont *builtinFontSubst[] = { &builtinFonts[0], &builtinFonts[3], &builtinFonts[1],  &builtinFonts[2],  &builtinFonts[4], &builtinFonts[7],
                                                  &builtinFonts[5], &builtinFonts[6], &builtinFonts[12], &builtinFonts[11], &builtinFonts[9], &builtinFonts[10] };

commit d2cc578a169f282fe5e3fb2e84b97dc11a81e820

Fix memory leak introduced in 2e3e7f80256023a7b7fafc921e889bef5e4fc0bd

fofi/FoFiType1C.cc
@@ -2261,7 +2261,7 @@ void FoFiType1C::buildEncoding()
         encoding = &fofiType1ExpertEncoding;
 
     } else {
-        auto *customEncoding = new std::array<const char *, 256>();
+        auto customEncoding = std::make_unique<std::array<const char *, 256>>();
         customEncoding->fill(nullptr);
         pos = topDict.encodingOffset;
         encFormat = getU8(pos++, &parsedOk);
@@ -2327,7 +2327,7 @@ void FoFiType1C::buildEncoding()
                 (*customEncoding)[c] = copyString(getString(sid, buf, &parsedOk));
             }
         }
-        encoding = customEncoding;
+        encoding = customEncoding.release();
     }
 }
 