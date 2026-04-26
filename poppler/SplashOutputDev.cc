e9fc2921f7feff9896192fc86dc5fe5b5b6e5338
Albert Astals Cid <aacid@kde.org>
2026-04-26 16:50:41 +0200

    convertGfxColor: We do not need the color variable

--- poppler/SplashOutputDev.cc
+++ poppler/SplashOutputDev.cc
@@ -89,46 +89,41 @@ static const double s_minLineWidth = 0.0;
 
 static inline void convertGfxColor(SplashColorPtr dest, const SplashColorMode colorMode, const GfxColorSpace *colorSpace, const GfxColor *src)
 {
-    SplashColor color;
     GfxGray gray;
     GfxRGB rgb;
     GfxCMYK cmyk;
     GfxColor deviceN;
 
-    // make gcc happy
-    color[0] = color[1] = color[2] = 0;
-    color[3] = 0;
     switch (colorMode) {
     case splashModeMono1:
     case splashModeMono8:
         colorSpace->getGray(src, &gray);
-        color[0] = colToByte(gray);
+        dest[0] = colToByte(gray);
         break;
     case splashModeXBGR8:
-        color[3] = 255;
+        dest[3] = 255;
         // fallthrough
     case splashModeBGR8:
     case splashModeRGB8:
         colorSpace->getRGB(src, &rgb);
-        color[0] = colToByte(rgb.r);
-        color[1] = colToByte(rgb.g);
-        color[2] = colToByte(rgb.b);
+        dest[0] = colToByte(rgb.r);
+        dest[1] = colToByte(rgb.g);
+        dest[2] = colToByte(rgb.b);
         break;
     case splashModeCMYK8:
         colorSpace->getCMYK(src, &cmyk);
-        color[0] = colToByte(cmyk.c);
-        color[1] = colToByte(cmyk.m);
-        color[2] = colToByte(cmyk.y);
-        color[3] = colToByte(cmyk.k);
+        dest[0] = colToByte(cmyk.c);
+        dest[1] = colToByte(cmyk.m);
+        dest[2] = colToByte(cmyk.y);
+        dest[3] = colToByte(cmyk.k);
         break;
     case splashModeDeviceN8:
         colorSpace->getDeviceN(src, &deviceN);
         for (int i = 0; i < SPOT_NCOMPS + 4; i++) {
-            color[i] = colToByte(deviceN.c[i]);
+            dest[i] = colToByte(deviceN.c[i]);
         }
         break;
     }
-    splashColorCopy(dest, color);
 }
 
 // Copy a color according to the color mode.
