a19b40e86f11ad538e51ad47ab50ed2ba9b31502
Albert Astals Cid <aacid@kde.org>
2026-05-18 23:46:04 +0200

    Update (C)

--- poppler/CairoFontEngine.h
+++ poppler/CairoFontEngine.h
@@ -23,6 +23,7 @@
 // Copyright (C) 2018 Adam Reichold <adam.reichold@t-online.de>
 // Copyright (C) 2022 Oliver Sander <oliver.sander@tu-dresden.de>
 // Copyright (C) 2022 Marek Kasik <mkasik@redhat.com>
+// Copyright (C) 2026 lilydjwg <lilydjwg@gmail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
