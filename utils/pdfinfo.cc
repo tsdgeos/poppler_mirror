0f212b125bdbf3080ce881133f0875ac7b6a1e03
Albert Astals Cid <aacid@kde.org>
2026-07-01 00:49:11 +0200

    Update (C)

--- utils/pdfinfo.cc
+++ utils/pdfinfo.cc
@@ -30,6 +30,7 @@
 // Copyright (C) 2019 Thomas Fischer <fischer@unix-ag.uni-kl.de>
 // Copyright (C) 2024-2026 g10 Code GmbH, Author: Sune Stolborg Vuorela <sune@vuorela.dk>
 // Copyright (C) 2025 Jonathan Hähne <jonathan.haehne@hotmail.com>
+// Copyright (C) 2026 evil rabbit <evilrabbit@tutamail.com>
 //
 // To see a description of the changes please see the Changelog file that
 // came with your tarball or type make ChangeLog if you are building from git
